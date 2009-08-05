bce932662e61d932ff3a3ef2e91a1ec78748d8c6
Daniel Dunbar <daniel@zuster.org>
2009-08-05 16:42:10 +0000

    Implement the comparable mixin for the InformativeMailNotifier, otherwise buildbot likes to infinite loop on reconfig.
    llvm-svn: 78210

--- zorg/buildbot/util/InformativeMailNotifier.py
+++ zorg/buildbot/util/InformativeMailNotifier.py
@@ -1,19 +1,22 @@
+import buildbot.util
 from buildbot.status import builder, mail
 
-class InformativeMailNotifier(mail.MailNotifier):
+class InformativeMailNotifier(mail.MailNotifier, buildbot.util.ComparableMixin):
     """MailNotifier subclass which provides additional information about the
     build failure inside the email."""
+
+    compare_attrs = ["num_lines", "only_failure_logs"]
 
     # FIXME: The customMessage interface is fairly inefficient, switch to
     # something new when it becomes available.
 
     def __init__(self, 
-                 numLines = 10, onlyFailureLogs = True,
+                 num_lines = 10, only_failure_logs = True,
                  *attrs, **kwargs):
         mail.MailNotifier.__init__(self, customMesg=self.customMessage, 
                                    *attrs, **kwargs)
-        self.numLines = numLines
-        self.onlyFailureLogs = onlyFailureLogs
+        self.num_lines = num_lines
+        self.only_failure_logs = only_failure_logs
 
     def customMessage(self, attrs):
         # Get the standard message.
@@ -27,14 +30,14 @@ class InformativeMailNotifier(mail.MailNotifier):
         data += '\n\n'
     
         # Append log files.
-        if self.numLines:
+        if self.num_lines:
             data += 'LOGS:\n'
             for name, url, lines, logstatus in attrs['logs']:
-                if (self.onlyFailureLogs and logstatus != builder.FAILURE):
+                if (self.only_failure_logs and logstatus != builder.FAILURE):
                     continue
 
-                data += "Last %d lines of '%s':\n" % (self.numLines, name)
-                data += '\t' + '\n\t'.join(lines[-self.numLines:])
+                data += "Last %d lines of '%s':\n" % (self.num_lines, name)
+                data += '\t' + '\n\t'.join(lines[-self.num_lines:])
                 data += '\n\n'
 
         return (data, 'plain')
