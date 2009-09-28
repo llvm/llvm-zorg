99f07fb6b1bde67ae8c761dff7843ae7ad90cd02
Daniel Dunbar <daniel@zuster.org>
2009-09-28 01:55:43 +0000

    Improve Clang (now 'lit') test parser to extract failure logs into individual reports.
    llvm-svn: 82961

--- zorg/buildbot/commands/ClangTestCommand.py
+++ zorg/buildbot/commands/ClangTestCommand.py
@@ -1,5 +1,6 @@
 import itertools
 import re
+import os
 
 import buildbot
 import buildbot.steps.shell
@@ -7,19 +8,42 @@ import buildbot.steps.shell
 class ClangTestCommand(buildbot.steps.shell.Test):
   description = "testing clang"
   descriptionDone = "test clang"
+
   kTestLineRE = re.compile(r'([^ ]*): (.*) \(.*.*\)')
+  kTestFailureLogStartRE = re.compile(r"""\*{4,80} TEST '(.*)' .*""")
+  kTestFailureLogStopRE = re.compile(r"""\*{10,80}""")
+
+  # Show a maximum of 20 individual failure logs.
+  kMaxFailureLogs = 20
+
   def evaluateCommand(self, cmd):
     rc = 0
     grouped = {}
-    failureLog = []
+    failureLogs = []
     lines = self.getLog('stdio').readlines()
 
     it = iter(lines)
+    inFailure = None
     for ln in it:
+      # See if we are inside a failure log.
+      if inFailure:
+        inFailure[1].append(ln)
+        if ClangTestCommand.kTestFailureLogStopRE.match(ln):
+          failureLogs.append(tuple(inFailure))
+          inFailure = None
+        continue
+
       ln = ln.strip()
       if not ln:
         continue
-      
+
+      # Check for test failure logs.
+      m = ClangTestCommand.kTestFailureLogStartRE.match(ln)
+      if m:
+        inFailure = (m.group(1), [ln])
+        continue
+
+      # Otherwise expect a test status line.
       m = ClangTestCommand.kTestLineRE.match(ln)
       if m:
         groupName = m.group(1)
@@ -28,10 +52,17 @@ class ClangTestCommand(buildbot.steps.shell.Test):
           grouped[groupName] = []
         grouped[groupName].append(testName)
 
-    # FIXME: Get logs for failures.
+    if inFailure:
+      # FIXME: Different error?
+      raise ValueError,"Unexpected clang test running output, unterminated failure log!"
+
     for name,items in grouped.items():
       if name != 'PASS' and items:
         self.addCompleteLog(name.lower(), '\n'.join(items) + '\n')
+    for name,items in failureLogs[:self.kMaxFailureLogs]:
+      self.addCompleteLog(os.path.basename(name.lower()),
+                          ''.join(items) + '\n')
+      
     numPass = len(grouped.get('PASS',()))
     numFail = len(grouped.get('FAIL',()))
     numXFail = len(grouped.get('XFAIL',()))
