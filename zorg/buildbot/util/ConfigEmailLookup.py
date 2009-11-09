ae62c1ad28b08d112e79ca91ffed72504a10b52c
Daniel Dunbar <daniel@zuster.org>
2009-11-09 06:08:36 +0000

    Add an optional only_addresses regular expression, which can be used to filter the email lookups (non-matches will go to the default address).
    llvm-svn: 86520

--- zorg/buildbot/util/ConfigEmailLookup.py
+++ zorg/buildbot/util/ConfigEmailLookup.py
@@ -7,20 +7,35 @@ class ConfigEmailLookup(buildbot.util.ComparableMixin):
   file to match commit authors to email addresses.
   """
 
-  zope.interface.implements(buildbot.interfaces.IEmailLookup)
-  compare_attrs = ["author_filename", "default_address"]
+  # FIXME: This should be able to reload the config file when it
+  # changes.
 
-  def __init__(self, author_filename, default_address):
+  zope.interface.implements(buildbot.interfaces.IEmailLookup)
+  compare_attrs = ["author_filename", "default_address", "only_addresses"]
+
+  def __init__(self, author_filename, default_address, only_addresses = None):
     from ConfigParser import ConfigParser
 
     self.author_filename = author_filename
     self.default_address = default_address
+    self.only_addresses = only_addresses
 
     self.config_parser = ConfigParser()
     self.config_parser.read(author_filename)
 
+    if only_addresses:
+      import re
+      self.address_match_p = re.compile(only_addresses).match
+    else:
+      self.address_match_p = lambda addr: True
+
   def getAddress(self, name):
     try:
-      return self.config_parser.get("authors", name)
+      email = self.config_parser.get("authors", name)
     except:
       return self.default_address
+
+    if self.address_match_p(email):
+      return email
+
+    return self.default_address
