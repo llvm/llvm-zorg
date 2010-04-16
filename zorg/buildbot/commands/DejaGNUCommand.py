e02b0da0d6e6d0a767e4b000d44fb3aed6b12437
Daniel Dunbar <daniel@zuster.org>
2010-04-16 23:36:12 +0000

    Switch DejaGNUCommand to use new StandardizedTest.
    llvm-svn: 101578

--- zorg/buildbot/commands/DejaGNUCommand.py
+++ zorg/buildbot/commands/DejaGNUCommand.py
@@ -1,56 +1,33 @@
 import re
-import buildbot
-import buildbot.status.builder
-from buildbot.steps.shell import Test
+import StandardizedTest
 
-class DejaGNUCommand(Test):
-    command=["make", "check"]
-    total = 0
-    description = "testing llvm"
-    descriptionDone = "test llvm"
-    def describe(self, done=False):
-        result = Test.describe(self, done)
-        return result
-    def evaluateCommand(self, cmd):
-        # FIXME: This doesn't detect failures if dejagnu doesn't run.
-        rc = 0
-        lines = self.getLog('stdio').readlines()
-        failre = re.compile("^FAIL:.*")
-        faillines = []
-        xpass_result = re.compile("^# of unexpected passes\s*(\d+)")
-        xpasses = 0
-        xfail_result = re.compile("^# of expected failures\s*(\d+)")
-        xfails = 0
-        fail_result = re.compile("^# of unexpected failures\s*(\d+)")
-        fails = 0
-        pass_result = re.compile("^# of expected passes\s*(\d+)")
-        passes = 0
-        fails_list = []
-        for line in lines:
-            result = failre.search(line)
-            if result:
-                faillines.append(line)
-            result =  xpass_result.search(line)
-            if result:
-                xpasses += int(result.group(1))
-            result =  xfail_result.search(line)
-            if result:
-                xfails += int(result.group(1))
-            result =  pass_result.search(line)
-            if result:
-                passes += int(result.group(1))
-            result =  fail_result.search(line)
-            if result:
-                fails += int(result.group(1))
-                fails_list.append(line)
-        if faillines:
-            self.addCompleteLog("fails", "\n".join(faillines) + "\n")
-        if fails == 0:
-            rc = buildbot.status.builder.SUCCESS
-        else:
-            rc = buildbot.status.builder.FAILURE
-        self.setTestResults(total = fails+xpasses+xfails+passes, 
-                            failed = fails, 
-                            passed = passes, 
-                            warnings = xpasses + xfails)
-        return rc
+class DejaGNUCommand(StandardizedTest.StandardizedTest):
+    kRunningRE = re.compile(r'Running (.*) ...')
+    kRunningRE = re.compile(r'Running (.*) ...')
+    kTestStateLineRE = re.compile(r'(FAIL|PASS|XFAIL|XPASS|UNRESOLVED): (.*)')
+
+    testLogName = 'dg.sum'
+
+    def parseLog(self, lines):
+        results = []
+
+        test_suite = None
+        for ln in lines:
+            m = self.kRunningRE.match(ln)
+            if m is not None:
+                test_suite, = m.groups()
+                continue
+
+            m = self.kTestStateLineRE.match(ln)
+            if m is not None:
+                code,name = m.groups()
+                results.append((code, name, None))
+                continue
+
+        return results
+
+if __name__ == '__main__':
+    import sys
+    t = DejaGNUCommand()
+    for res in t.parseLog(open(sys.argv[1]).readlines()):
+        print res
