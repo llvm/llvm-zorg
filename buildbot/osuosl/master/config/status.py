1349885d6349f6e4c4ef2554ea08f0c8c2161840
Galina Kistanova <gkistanova@gmail.com>
2010-06-26 00:37:39 +0000

    Added e-mail notifier for dragonegg builders which sends the last 15 lines of stdio
    llvm-svn: 106933

--- buildbot/osuosl/master/config/status.py
+++ buildbot/osuosl/master/config/status.py
@@ -6,6 +6,7 @@ import buildbot.status.words
 
 import config
 from zorg.buildbot.util.ConfigEmailLookup import ConfigEmailLookup
+from zorg.buildbot.util.InformativeMailNotifier import InformativeMailNotifier 
 
 def get_status_targets(standard_builders):
     default_email = config.options.get('Master Options', 'default_email')
@@ -24,4 +25,13 @@ def get_status_targets(standard_builders):
             host = "irc.oftc.net", nick = "llvmbb", channels = ["#llvm"],
             allowForce = True,
             notify_events = ['successToFailure', 'failureToSuccess']),
+        InformativeMailNotifier(
+            fromaddr = "buildbot@google1.osuosl.org",
+            sendToInterestedUsers= False,
+            extraRecipients = ["baldrick@free.fr", "gkistanova@gmail.com"],
+            subject="Build %(builder)s Failure",
+            mode = "failing",
+            builders = ["dragonegg-i386-linux", "dragonegg-x86_64-linux"],
+            addLogs=False,
+            num_lines = 15),
         ]
