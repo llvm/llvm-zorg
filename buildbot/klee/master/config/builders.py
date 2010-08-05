9970072e0ed3e688e21b11dd3227a92fda477205
Daniel Dunbar <daniel@zuster.org>
2010-08-05 15:40:55 +0000

    buildbot: Add KLEE with LLVM 2.6 and LLVM 2.7 builders.
    llvm-svn: 110336

--- buildbot/klee/master/config/builders.py
+++ buildbot/klee/master/config/builders.py
@@ -1,13 +1,29 @@
-from zorg.buildbot.builders import LLVMBuilder
-reload(LLVMBuilder)
-from zorg.buildbot.builders import LLVMBuilder
+from zorg.buildbot.builders import KLEEBuilder
+reload(KLEEBuilder)
+from zorg.buildbot.builders import KLEEBuilder
 
 def _get_linux_builders():
     return [
-        {'name': "klee-x86_64-linux",
-         'slavenames': ["klee.minormatter.com"],
-         'builddir': "build.klee-x86_64-linux", 
-         'factory': LLVMBuilder.getLLVMBuildFactory("x86_64-pc-linux-gnu", jobs=2)},
+        { 'name'       : "klee-x86_64-linux",
+          'slavenames' : ["klee.minormatter.com"],
+          'builddir'   : "build.klee-x86_64-linux", 
+          'factory'    : KLEEBuilder.getKLEEBuildFactory("x86_64-pc-linux-gnu", clean=False) },
+
+        { 'name'       : "klee-2.7-x86_64-linux",
+          'slavenames' : ["klee.minormatter.com"],
+          'builddir'   : "build.klee-2.7-x86_64-linux", 
+          'factory'    : KLEEBuilder.getKLEEBuildFactory(
+                "x86_64-pc-linux-gnu", clean=False,
+                llvm_branch='tags/RELEASE_27',
+                llvmgccdir='/home/klee-buildslave/llvm-gcc-4.2-2.7-x86_64-linux') },
+
+        { 'name'       : "klee-2.6-x86_64-linux",
+          'slavenames' : ["klee.minormatter.com"],
+          'builddir'   : "build.klee-2.6-x86_64-linux", 
+          'factory'    : KLEEBuilder.getKLEEBuildFactory(
+                "x86_64-pc-linux-gnu", clean=False,
+                llvm_branch='tags/RELEASE_26',
+                llvmgccdir='/home/klee-buildslave/llvm-gcc-4.2-2.6-x86_64-linux') },
         ]
 
 def get_builders():
