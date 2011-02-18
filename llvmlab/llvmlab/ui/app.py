8a6b944be51176816bde2e9a74ea4977668e9e6c
Daniel Dunbar <daniel@zuster.org>
2011-02-18 16:40:42 +0000

    llvmlab: Add login support.  - Not yet very secure, password goes in plain text (TODOed).
    llvm-svn: 125861

--- llvmlab/llvmlab/ui/app.py
+++ llvmlab/llvmlab/ui/app.py
@@ -1,7 +1,8 @@
+import hashlib
 import os
 
 import flask
-from flask import redirect, render_template, url_for
+from flask import redirect, render_template, request, session, url_for
 
 import llvmlab.data
 import llvmlab.user
@@ -29,6 +30,11 @@ def load_llvmlab_data(app):
 
     return data
 
+def authenticate_login(username, password):
+    passhash = hashlib.sha256(password + app.config["SECRET_KEY"]).hexdigest()
+    user = app.config.data.users.get(username)
+    return user and passhash == user.passhash
+
 ###
 
 # Construct the Flask application.
@@ -36,6 +42,9 @@ app = flask.Flask(__name__)
 
 # Load the configuration file.
 app.config.from_envvar("LLVMLAB_CONFIG")
+
+# Set the application secret key.
+app.secret_key = app.config["SECRET_KEY"]
 
 # Load the LLVM-Lab database.
 app.config.data = load_llvmlab_data(app)
@@ -54,6 +63,31 @@ def favicon_ico():
 @app.route('/users')
 def users():
     return render_template("users.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # If this isn't a post request, return the login template.
+    if request.method != 'POST':
+        return render_template("login.html", error=None)
+
+    # Authenticate the user.
+    username = request.form['username']
+    if not authenticate_login(username, request.form['password']):
+        return render_template("login.html",
+                               error="Invalid login")
+
+    # Log the user in.
+    session['logged_in'] = True
+    session['active_user'] = username
+    flask.flash('You were logged in as "%s"!' % username)
+    return redirect(url_for("index"))
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    session.pop('active_user', None)
+    flask.flash('You were logged out!')
+    return redirect(url_for("index"))
 
 ###
 
