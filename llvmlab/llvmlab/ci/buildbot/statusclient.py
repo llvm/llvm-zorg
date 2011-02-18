6a5a36fa6e6308efd1db4db4f148c03f1bc986e6
Daniel Dunbar <daniel@zuster.org>
2011-02-18 16:42:43 +0000

    llvmlab.ci.buildbot.statusclient: Factor out a BuilderInfo object.
    llvm-svn: 125901

--- llvmlab/llvmlab/ci/buildbot/statusclient.py
+++ llvmlab/llvmlab/ci/buildbot/statusclient.py
@@ -2,6 +2,31 @@ import time
 import urllib2
 from flask import json
 
+class BuilderInfo(object):
+    """
+    BuilderInfo object for tracking per-builder status information being
+    monitored.
+    """
+
+    @staticmethod
+    def fromdata(data):
+        version = data['version']
+        if version != 0:
+            raise ValueError, "Unknown version"
+
+        return BuilderInfo(data['name'], set(data['builds']), data['last_poll'])
+
+    def todata(self):
+        return { 'version' : 0,
+                 'name' : self.name,
+                 'builds' : list(self.builds),
+                 'last_poll' : self.last_poll }
+
+    def __init__(self, name, builds, last_poll):
+        self.name = name
+        self.builds = builds
+        self.last_poll = last_poll
+
 class StatusClient(object):
     """
     StatusClient object for watching a buildbot master and dispatching signals
@@ -18,11 +43,10 @@ class StatusClient(object):
 
         sc = StatusClient(data['master_url'], data['builders_poll_rate'],
                           data['builder_poll_rate'])
-        sc.builders = set(data['builders'])
-        sc.builds = dict((name, set(items))
-                         for name, items in data['builds'])
+        builders = [BuilderInfo.fromdata(b)
+                    for b in data['builders']]
+        sc.builders = dict((b.name, b) for b in builders)
         sc.last_builders_poll = data['last_builders_poll']
-        sc.last_builder_poll = data['last_builder_poll']
         return sc
 
     def todata(self):
@@ -30,11 +54,9 @@ class StatusClient(object):
                  'master_url' : self.master_url,
                  'builder_poll_rate' : self.builder_poll_rate,
                  'builders_poll_rate' : self.builders_poll_rate,
-                 'builders' : list(self.builders),
-                 'builds' : [(name, list(items))
-                            for name,items in self.builds.items()],
-                 'last_builders_poll' : self.last_builders_poll,
-                 'last_builder_poll' : self.last_builder_poll }
+                 'builders' : [b.todata()
+                               for b in self.builders.values()],
+                 'last_builders_poll' : self.last_builders_poll }
 
     def __init__(self, master_url,
                  builders_poll_rate = 60.0,
@@ -45,17 +67,15 @@ class StatusClient(object):
             self.master_url += '/'
 
         # Initialize the data we track.
-        self.builders = set()
-        self.builds = {}
+        self.builders = {}
 
         # Set poll rates (how frequently we are willing to recontact the
         # master).
         self.builders_poll_rate = float(builders_poll_rate)
         self.builder_poll_rate = float(builder_poll_rate)
 
-        # Set last poll times so we will repoll on startup.
+        # Set last poll time so we will repoll on startup.
         self.last_builders_poll = -1
-        self.last_builder_poll = {}
 
     def get_json_result(self, query_items, arguments=None):
         path = '/json/' + '/'.join(urllib2.quote(item)
@@ -81,62 +101,43 @@ class StatusClient(object):
                 yield event
 
         # Update the current builds for each known builder.
-        for builder in self.builders:
-            last_poll = self.last_builder_poll.get(builder, -1)
-            if current_time - last_poll >= self.builder_poll_rate:
+        for builder in self.builders.values():
+            if current_time - builder.last_poll >= self.builder_poll_rate:
                 for event in self.pull_builder(builder):
                     yield event
-
-    def add_builder(self, name):
-        yield ('added_builder', name)
-        self.builders.add(name)
-        self.last_builder_poll[name] = -1
-        self.builds[name] = set()
-
-    def remove_builder(self, name):
-        yield ('removed_builder', name)
-        self.builders.remove(name)
-        self.last_builder_poll.pop(name)
-        self.builds.pop(name)
-
-    def add_build(self, name, id):
-        yield ('add_build', name, id)
-        self.builds[name].add(id)
-    def remove_build(self, name, id):
-        yield ('remove_build', name, id)
-        self.builds[name].remove(id)
 
     def pull_builders(self):
         # Pull the builder names.
         #
         # FIXME: BuildBot should provide a more efficient query for this.
         yield ('poll_builders',)
-        builders = self.get_json_result(('builders',))
-        builder_names = set(builders.keys())
+        res = self.get_json_result(('builders',))
+        builder_names = set(res.keys())
+        current_builders = set(self.builders)
 
-        for name in builder_names - self.builders:
-            for event in self.add_builder(name):
-                yield event
-        for name in self.builders - builder_names:
-            for event in self.remove_builder(name):
-                yield event
+        for name in builder_names - current_builders:
+            yield ('added_builder', name)
+            self.builders[name] = BuilderInfo(name, set(), -1)
+        for name in current_builders - builder_names:
+            yield ('removed_builder', name)
+            self.builders.remove(name)
 
         self.last_builders_poll = time.time()
 
-    def pull_builder(self, name):
+    def pull_builder(self, builder):
         # Pull the builder data.
-        yield ('poll_builder', name)
-        builder = self.get_json_result(('builders', name))
-        builds = set(builder['cachedBuilds'])
+        yield ('poll_builder', builder.name)
+        res = self.get_json_result(('builders', builder.name))
+        builds = set(res['cachedBuilds'])
 
-        for id in builds - self.builds[name]:
-            for event in self.add_build(name, id):
-                yield event
-        for id in self.builds[name] - builds:
-            for event in self.remove_build(name, id):
-                yield event
+        for id in builds - builder.builds:
+            yield ('add_build', builder.name, id)
+            builder.builds.add(id)
+        for id in builder.builds - builds:
+            yield ('remove_build', builder.name, id)
+            builder.builds.remove(id)
 
-        self.last_builder_poll[name] = time.time()
+        builder.last_poll = time.time()
 
 ###
 
