0dcbb4a2a8368598e7cacdd453b0c0ad69db0e9f
Daniel Dunbar <daniel@zuster.org>
2011-02-18 16:40:11 +0000

    llvmlab: Add a favicon.
    Also, use debugger by default when started from command line.
    
    llvm-svn: 125850

--- llvmlab/llvmlab/ui/app.py
+++ llvmlab/llvmlab/ui/app.py
@@ -1,9 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+from flask import redirect, url_for
+
+# Construct the Flask application.
+app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
     return "Ceci n'est pas un laboratoire."
 
+@app.route('/favicon.ico')
+def favicon_ico():
+    return redirect(url_for('static', filename='favicon.ico'))
+
 if __name__ == '__main__':
+    app.debug = True
     app.run()
