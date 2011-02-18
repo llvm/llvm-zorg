f52b6fe6abfb914e5222df29e9097623662fe0b0
Daniel Dunbar <daniel@zuster.org>
2011-02-18 16:41:41 +0000

    llvmlab: Continue sketching published builds part of hard-coded instance of the Config object (with dashboard support).
    llvm-svn: 125880

--- llvmlab/llvmlab/ui/ci/views.py
+++ llvmlab/llvmlab/ui/ci/views.py
@@ -22,7 +22,22 @@ phases = [
     config.Phase("Tree Health", 3, []),
     config.Phase("Validation", 4, [])]
 builders = []
-published_builds = []
+published_builds = [
+    config.PublishedBuild("LLVM", "Linux", "i386", "llvm-linux-i386.tgz"),
+    config.PublishedBuild("LLVM", "Mac OS X (SnowLeopard)", "x86_64",
+                          "llvm-darwin10-x86_64.tgz"),
+
+    config.PublishedBuild("Clang", "Linux", "i386", "clang-linux-i386.tgz"),
+    config.PublishedBuild("Clang", "Linux", "x86_64", "clang-linux-x86_64.tgz"),
+    config.PublishedBuild("Clang", "Mac OS X (SnowLeopard)", "x86_64",
+                          "clang-darwin10-x86_64.tgz"),
+    config.PublishedBuild("Clang", "Windows", "i386", "clang-windows-i386.tgz"),
+
+    config.PublishedBuild("llvm-gcc-4.2", "Linux", "i386",
+                          "llvm-gcc-4.2-linux-i386.tgz"),
+    config.PublishedBuild("llvm-gcc-4.2", "Linux", "x86_64",
+                          "llvm-gcc-4.2-linux-x86_64.tgz"),
+    ]
 g_config = config.Config(phases, builders, published_builds)
 
 @ci.route('/')
