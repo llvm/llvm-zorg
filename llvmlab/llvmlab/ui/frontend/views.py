a28395f1567dfdd50a9360f4a22f3cbab6d7bd5b
Daniel Dunbar <daniel@zuster.org>
2011-02-18 16:42:40 +0000

    llvmlab: Add admin page to add a machine.
    llvm-svn: 125900

--- llvmlab/llvmlab/ui/frontend/views.py
+++ llvmlab/llvmlab/ui/frontend/views.py
@@ -6,10 +6,14 @@ from flask import request
 from flask import session
 from flask import url_for
 from flask import current_app
-frontend = flask.Module(__name__)
+
+import llvmlab
+import llvmlab.machine
 
 ###
 # Top-level Information
+
+frontend = flask.Module(__name__)
 
 @frontend.route('/')
 def index():
@@ -25,6 +29,44 @@ def admin():
 
 ###
 # Machine Management
+
+@frontend.route('/add_machine', methods=['GET', 'POST'])
+def add_machine():
+    # Check that we have an active user.
+    user = current_app.get_active_user()
+    if user is None:
+        abort(401)
+
+    # Check that the user has authority to access the lab.
+    if not user.has_lab_access():
+        abort(401)
+
+    # If this isn't a post request, return the template.
+    if request.method != 'POST':
+        return render_template("add_machine.html", error=None)
+
+    # Validate the entry data.
+    id = request.form['id']
+    hostname = request.form['hostname']
+    if id in current_app.config.data.machines:
+        return render_template("add_machine.html",
+                               error='machine "%s" already exists' % id)
+    if hostname in set(m.hostname
+                       for m in current_app.config.data.machines.values()):
+        return render_template("add_machine.html",
+                               error='hostname "%s" already used' % hostname)
+
+    # Add the machine record.
+    machine = llvmlab.machine.Machine(id, hostname, user.id)
+    current_app.config.data.machines[machine.id] = machine
+    flask.flash('Added machine "%s"!' % machine.id)
+
+    # Save the application data.
+    current_app.save_data()
+    flask.flash('Saved data!')
+
+    return redirect(url_for("admin"))
+
 @frontend.route('/machines')
 def machines():
     return render_template("machines.html")
