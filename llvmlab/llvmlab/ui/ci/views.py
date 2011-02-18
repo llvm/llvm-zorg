c9a097589150e7987a4cf039261f9b20eab854fc
Daniel Dunbar <daniel@zuster.org>
2011-02-18 16:41:38 +0000

    llvmlab: Start sketching a hard-coded instance of the Config object, and migrate dashboard to using the actual Config.
    llvm-svn: 125879

--- llvmlab/llvmlab/ui/ci/views.py
+++ llvmlab/llvmlab/ui/ci/views.py
@@ -7,9 +7,25 @@ from flask import session
 from flask import url_for
 from flask import current_app
 ci = flask.Module(__name__, url_prefix='/ci')
-print __name__
+
+from llvmlab.ci import config
+
+# Hard-coded current configuration.
+#
+# FIXME: Figure out how to get as much of this as possible dynamically. One
+# problem is how do we deal with changes to the CI infrastructure? Should we
+# load a config object per-revision, and try to be smart about caching it unless
+# things change? Can we report results across changing configs?
+phases = [
+    config.Phase("Sanity", 1, []),
+    config.Phase("Living On", 2, []),
+    config.Phase("Tree Health", 3, []),
+    config.Phase("Validation", 4, [])]
+builders = []
+published_builds = []
+g_config = config.Config(phases, builders, published_builds)
 
 @ci.route('/')
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", ci_config=g_config)
 
