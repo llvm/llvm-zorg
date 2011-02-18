6f239e2d11c8f520e4e174dd4378579a49078982
Daniel Dunbar <daniel@zuster.org>
2011-02-18 16:41:44 +0000

    llvmlab: Fill in builders list in hardcoded Config.
    llvm-svn: 125881

--- llvmlab/llvmlab/ui/ci/views.py
+++ llvmlab/llvmlab/ui/ci/views.py
@@ -21,7 +21,23 @@ phases = [
     config.Phase("Living On", 2, []),
     config.Phase("Tree Health", 3, []),
     config.Phase("Validation", 4, [])]
-builders = []
+builders = [
+    config.Builder("clang-x86_64-osx10-gcc42-RA"),
+    config.Builder("clang-x86_64-osx10-DA"),
+    config.Builder("clang-x86_64-osx10-RA"),
+    config.Builder("nightly_clang-x86_64-osx10-gcc42-RA"),
+    config.Builder("nightly_clang-x86_64-osx10-DA"),
+    config.Builder("nightly_clang-x86_64-osx10-RA"),
+    config.Builder("nightly_clang-x86_64-osx10-RA-O0"),
+    config.Builder("nightly_clang-x86_64-osx10-RA-Os"),
+    config.Builder("nightly_clang-x86_64-osx10-RA-O3"),
+    config.Builder("nightly_clang-x86_64-osx10-RA-flto"),
+    config.Builder("nightly_clang-x86_64-osx10-RA-g"),
+    config.Builder("clang-x86_64-osx10-RA-stage3"),
+    config.Builder("gccTestSuite-clang-x86_64-osx10-RA"),
+    config.Builder("nightly_clang-x86_64-osx10-RA-stage3-g"),
+    config.Builder("libcxx-clang-x86_64-osx10-RA"),
+    config.Builder("boost-trunk-clang-x86_64-osx10-RA")]
 published_builds = [
     config.PublishedBuild("LLVM", "Linux", "i386", "llvm-linux-i386.tgz"),
     config.PublishedBuild("LLVM", "Mac OS X (SnowLeopard)", "x86_64",
@@ -36,8 +52,7 @@ published_builds = [
     config.PublishedBuild("llvm-gcc-4.2", "Linux", "i386",
                           "llvm-gcc-4.2-linux-i386.tgz"),
     config.PublishedBuild("llvm-gcc-4.2", "Linux", "x86_64",
-                          "llvm-gcc-4.2-linux-x86_64.tgz"),
-    ]
+                          "llvm-gcc-4.2-linux-x86_64.tgz")]
 g_config = config.Config(phases, builders, published_builds)
 
 @ci.route('/')
