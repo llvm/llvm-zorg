964a551396a2bcac16106181ec02b7ac9cb70885
Daniel Dunbar <daniel@zuster.org>
2011-02-25 16:45:27 +0000

    llvmlab: Add a 'ci/build_chart' which shows a Gantt chart of where builds were done of the past N days.
    llvm-svn: 126493

--- llvmlab/llvmlab/ui/ci/views.py
+++ llvmlab/llvmlab/ui/ci/views.py
@@ -47,6 +47,70 @@ def buildbot_monitor():
     return render_template("buildbot_monitor.html",
                            bb_status=current_app.config.status)
 
+@ci.route('/build_chart')
+def build_chart():
+    import time
+
+    # Determine the render constants.
+    k_days_data = int(request.args.get('days', 1))
+    k_pixels_per_minute = float(request.args.get('pixels_per_minute', .5))
+
+    # Aggregate builds by slave, for completed builds within the desired time
+    # frame.
+    current_time = time.time()
+    builders = current_app.config.status.builders
+    slave_builders = util.multidict(
+        (build.slave, build)
+        for builds in builders.values()
+        for build in builds
+        if build.end_time is not None
+        if current_time - build.start_time < 60 * 60 * 24 * k_days_data)
+
+    # Compute the build chart.
+    class ChartItem(object):
+        def __init__(self, build, color, left, width):
+            self.build = build
+            self.color = color
+            self.left = left
+            self.width = width
+    builder_colors = dict((name, util.make_dark_color(float(i) / len(builders)))
+                          for i,name in enumerate(builders))
+    build_chart_data = {}
+    max_x = 0
+    min_time = min(build.start_time
+                   for builds in slave_builders.values()
+                   for build in builds)
+    for slave, builders in slave_builders.items():
+        # Order the builders by time.
+        builders.sort(key = lambda b: b.start_time)
+        
+        # Aggregate builds by builder type.
+        builds_by_type = util.multidict(
+            (build.name, build)
+            for build in builders)
+
+        # Create the char items.
+        rows = []
+        for name,builds in util.sorted(builds_by_type.items()):
+            color = builder_colors[name]
+            hex_color = '%02x%02x%02x' % tuple(int(x*255)
+                                           for x in color)
+            rows.append([])
+            for build in builds:
+                elapsed = build.end_time - build.start_time
+                width = max(1, int(k_pixels_per_minute * elapsed / 60))
+                left = int(k_pixels_per_minute *
+                           (build.start_time - min_time) / 60)
+                max_x = max(max_x, left + width)
+                rows[-1].append(ChartItem(build, hex_color, left, width))
+        build_chart_data[slave] = rows
+
+    build_chart = { 'data' : build_chart_data,
+                    'max_x' : max_x }
+    return render_template("build_chart.html",
+                           bb_status = current_app.config.status,
+                           build_chart = build_chart)
+
 @ci.route('/phase_description/<int:index>')
 def phase_description(index):
     cfg = current_app.config.summary.config
