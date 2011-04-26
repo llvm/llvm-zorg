0e1173b2db83728c498b6db15e5ca29e62c0620d
Daniel Dunbar <daniel@zuster.org>
2011-04-26 17:47:49 +0000

    LNT/Flask: Finish porting index page.
    llvm-svn: 130212

--- lnt/lnt/server/ui/app.py
+++ lnt/lnt/server/ui/app.py
@@ -5,6 +5,7 @@ import time
 
 import flask
 from flask import current_app
+from flask import g
 
 import lnt
 import lnt.server.ui.filters
@@ -12,15 +13,40 @@ import lnt.server.ui.views
 
 # FIXME: Redesign this.
 import lnt.viewer.Config
+from lnt.db import perfdbsummary
+from lnt.viewer import PerfDB
 
 class Request(flask.Request):
     def __init__(self, *args, **kwargs):
         super(Request, self).__init__(*args, **kwargs)
 
         self.request_time = time.time()
+        self.db = None
+        self.db_summary = None
 
     def elapsed_time(self):
         return time.time() - self.request_time
+
+    # Utility Methods
+
+    def get_db(self):
+        if self.db is None:
+            self.db = PerfDB.PerfDB(g.db_info.path)
+
+            # Enable SQL logging with db_log.
+            #
+            # FIXME: Conditionalize on an is_production variable.
+            if self.args.get('db_log') or self.form.get('db_log'):
+                import logging, StringIO
+                g.db_log = StringIO.StringIO()
+                logger = logging.getLogger("sqlalchemy")
+                logger.addHandler(logging.StreamHandler(g.db_log))
+                self.db.engine.echo = True
+
+        return self.db
+
+    def get_db_summary(self):
+        return current_app.get_db_summary(g.db_name, self.get_db())
 
 class App(flask.Flask):
     @staticmethod
@@ -42,6 +68,7 @@ class App(flask.Flask):
     def __init__(self, name):
         super(App, self).__init__(name)
         self.start_time = time.time()
+        self.db_summaries = {}
 
         # Override the request class.
         self.request_class = Request
@@ -59,3 +86,11 @@ class App(flask.Flask):
         self.jinja_env.globals.update(
             app=current_app,
             old_config=self.old_config)
+
+    def get_db_summary(self, db_name, db):
+        db_summary = self.db_summaries.get(db_name)
+        if db_summary is None or not db_summary.is_up_to_date(db):
+            self.db_summaries[db_name] = db_summary = \
+                perfdbsummary.PerfDBSummary.fromdb(db)
+        return db_summary
+
