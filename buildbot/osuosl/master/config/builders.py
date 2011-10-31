1140590f5b6e83297b7737dbaa01ec4fed4d17e5
Peter Collingbourne <peter@pcc.me.uk>
2011-10-31 00:49:38 +0000

    LLDB: add more environment variables for 64-bit builder, and disable 32-bit builder
    gcc15 lacks enough RAM to build clang or lldb in a reasonable amount
    of time.
    
    llvm-svn: 143325

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -365,25 +365,28 @@ def _get_polly_builders():
 # LLDB builders.
 def _get_lldb_builders():
     gcc_latest_env = {
-        'PATH': '/opt/cfarm/python2-latest/bin:/usr/local/bin:/usr/bin:/bin:/usr/games',
+        'PATH': '/home/baldrick/python-shared/bin/Python-2.7.2/bin:/usr/local/bin:/usr/bin:/bin:/usr/games',
+        'LD_LIBRARY_PATH': '/opt/cfarm/release/4.5.1/lib64:/home/baldrick/python-shared/bin/Python-2.7.2/lib',
+        'CPATH': '/home/baldrick/libedit-dev/usr/include',
+        'LIBRARY_PATH': '/home/baldrick/libedit-dev/usr/lib:/home/baldrick/python-shared/bin/Python-2.7.2/lib',
         'CC':  '/opt/cfarm/release/4.5.1/bin/gcc',
         'CXX': '/opt/cfarm/release/4.5.1/bin/g++'}
 
-    gcc_m32_latest_env = gcc_latest_env.copy()
-    gcc_m32_latest_env['CC'] += ' -m32'
-    gcc_m32_latest_env['CXX'] += ' -m32'
-
+#   gcc_m32_latest_env = gcc_latest_env.copy()
+#   gcc_m32_latest_env['CC'] += ' -m32'
+#   gcc_m32_latest_env['CXX'] += ' -m32'
+#
     return [
         {'name': "lldb-x86_64-linux",
          'slavenames': ["gcc14"],
          'builddir': "lldb-x86_64",
          'factory': LLDBBuilder.getLLDBBuildFactory(triple="x86_64-pc-linux-gnu",
                                                     env=gcc_latest_env)},
-        {'name': "lldb-i686-debian",
-         'slavenames': ["gcc15"],
-         'builddir': "lldb-i686-debian",
-         'factory': LLDBBuilder.getLLDBBuildFactory(triple="i686-pc-linux-gnu",
-                                                    env=gcc_m32_latest_env)}
+#       {'name': "lldb-i686-debian",
+#        'slavenames': ["gcc15"],
+#        'builddir': "lldb-i686-debian",
+#        'factory': LLDBBuilder.getLLDBBuildFactory(triple="i686-pc-linux-gnu",
+#                                                   env=gcc_m32_latest_env)}
        ]
 
 def _get_experimental_builders():
