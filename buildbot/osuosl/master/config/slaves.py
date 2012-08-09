aafc48aabbe8fbf22bd4f1f57c70421622f2dc63
Galina Kistanova <gkistanova@gmail.com>
2012-08-09 18:14:35 +0000

    Patch by Stephen Schiffli! Add new builder clang-native-mingw64-win7.
    llvm-svn: 161599

--- buildbot/osuosl/master/config/slaves.py
+++ buildbot/osuosl/master/config/slaves.py
@@ -147,6 +147,9 @@ def get_build_slaves():
         # 2005 PowerPC Mac Mini, Mac OS X 10.5
         create_slave("arxan_bellini", properties={'jobs': 2}, max_builds=1),
 
+        # Intel(R) Core i7 920 @ 2.8GHz, 6 GB RAM, Windows 7 x64, Mingw64
+        create_slave("sschiffli1", properties={'jobs': 4}, max_builds=1),
+
         # POWER7 PowerPC (powerpc64), Fedora16.
         create_slave("chinook", properties={'jobs': 12}, max_builds=4),
 
