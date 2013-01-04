d86ef8e85a2c86eee4e2fc7c8bf2a3696f92d901
David Dean <david_dean@apple.com>
2013-01-04 23:56:44 +0000

    Loading of local options needs to happen earlier here.
    llvm-svn: 171562

--- buildbot/osuosl/master/config/__init__.py
+++ buildbot/osuosl/master/config/__init__.py
@@ -1,9 +1,10 @@
-import builders
-import slaves
-import status
-
 # Load local options.
 import os
 import ConfigParser
 options = ConfigParser.RawConfigParser()
 options.read(os.path.join(os.path.dirname(__file__), 'local.cfg'))
+
+import builders
+import slaves
+import status
+
