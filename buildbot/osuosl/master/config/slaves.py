5c5e53bd7bae438f69e74ef982fc40766bd42ce7
Galina Kistanova <gkistanova@gmail.com>
2013-01-26 01:40:40 +0000

    Added one more slave for clang-native-arm-cortex-a9 builder.
    llvm-svn: 173551

--- buildbot/osuosl/master/config/slaves.py
+++ buildbot/osuosl/master/config/slaves.py
@@ -23,6 +23,7 @@ def get_build_slaves():
         # Ubuntu pandaboard cortex-a9 slaves
         create_slave("as-bldslv1", properties={'jobs' : 2}, max_builds=1),
         create_slave("as-bldslv2", properties={'jobs' : 2}, max_builds=1),
+        create_slave("as-bldslv3", properties={'jobs' : 2}, max_builds=1),
         create_slave("linaro-panda-01", properties={'jobs' : 2}, max_builds=1),
 
         # FreeBSD 8.2 X86_64
