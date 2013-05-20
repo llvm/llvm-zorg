d918697c9ecac3c455d644333d3befb0a817f233
David Dean <david_dean@apple.com>
2013-05-20 21:54:23 +0000

    Add a build factory for the gcc test suites for the phased buildmaster.
    llvm-svn: 182322

---
+++ zorg/buildbot/builders/gccSuiteBuilder.py
@@ -0,0 +1,27 @@
+import os
+import buildbot
+from zorg.buildbot.PhasedBuilderUtils import getBuildDir, setProperty
+from zorg.buildbot.Artifacts import GetCompilerArtifacts
+import zorg.buildbot.builders.ClangBuilder as ClangBuilder
+
+def gccRunSuite(languages):
+    f = buildbot.process.factory.BuildFactory()
+    # Determine the build directory.
+    getBuildDir(f)
+    # Download compiler from upstream builder.
+    GetCompilerArtifacts(f)
+    # Load the ignore set.
+    #
+    # FIXME: This is lame, it is only loaded at startup.
+    ignores = ClangBuilder.getClangTestsIgnoresFromPath(
+        os.path.expanduser('~/public/clang-tests'),
+        'clang-x86_64-darwin10')
+    # Convert languages to GCC style names.
+    languages = [{'c' : 'gcc', 'c++' : 'g++', 'obj-c' : 'objc',
+                  'obj-c++' : 'obj-c++'}[l]
+                 for l in languages]
+    # Run gcc test suite.
+    ClangBuilder.addClangGCCTests(f, ignores,
+                                  install_prefix = '%(builddir)s/host-compiler',
+                                  languages = languages)
+    return f
