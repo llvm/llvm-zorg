1e19ee1e505643989a4cccfd9d57a2515a373ce1
Michael Gottesman <mgottesman@apple.com>
2013-08-18 09:14:05 +0000

    [llvmlab] Refactored some parts of Artifacts.py.
    llvm-svn: 188631

--- zorg/buildbot/Artifacts.py
+++ zorg/buildbot/Artifacts.py
@@ -6,17 +6,6 @@ from zorg.buildbot.PhasedBuilderUtils import setProperty, determine_phase_id
 from zorg.buildbot.PhasedBuilderUtils import set_config_option
 
 # Get some parameters about where to upload and download results from.
-# Set up defaults assuming we aren't in  production mode which
-# assume that we are just using a local user.
-import getpass
-rsync_user = getpass.getuser()
-master_name = 'localhost'
-master_protocol = 'http'
-base_download_url = 'http://%s/~%s/artifacts' % (master_name, rsync_user)
-package_url = 'http://%s/~%s/packages' % (master_name, rsync_user)
-base_rsync_path = '%s@%s:~/artifacts' % (rsync_user , master_name)
-curl_flags = '-fvLo'
-
 is_production = set_config_option('Master Options', 'is_production')
 if is_production:
     rsync_user = set_config_option('Master Options', 'rsync_user',
@@ -29,11 +18,21 @@ if is_production:
     base_package_url = '%s://%s/packages' % (master_protocol, master_name)
     package_url = set_config_option('Master Options', 'package_url',
                                     base_package_url)
-    base_rsync_path = set_config_option('Master Options', 'base_rsync_path',
-                                        '%s@%s:~/artifacts' %
-                                        (rsync_user , master_name))
+    artifacts_path = set_config_option('Master Options', 'artifacts_path',
+                                       '~/artifacts')
     curl_flags = set_config_option('Master Options', 'curl_flags',
-                                   '-fvLo')
+                                   '-fvLo')    
+else:
+    import getpass
+    rsync_user = getpass.getuser()
+    master_name = 'localhost'
+    master_protocol = 'http'
+    base_download_url = 'http://%s/~%s/artifacts' % (master_name, rsync_user)
+    package_url = 'http://%s/~%s/packages' % (master_name, rsync_user)
+    artifacts_path = '~/artifacts'
+    curl_flags = '-fvLo'
+
+base_rsync_path = '%s@%s:%s' % (rsync_user, master_name, artifacts_path)
 
 # This method is used in determining the name of a given compiler archive
 def _determine_compiler_kind(props):
