aeae0e6baf82ecc96e2f3c071ad388396a212d20
Michael Gottesman <mgottesman@apple.com>
2013-08-20 04:48:29 +0000

    [llvmlab] Force the lto builder to use 2 threads to reduce swapping issues.
    llvm-svn: 188744

--- buildbot/llvmlab/master/config/slaves.py
+++ buildbot/llvmlab/master/config/slaves.py
@@ -23,7 +23,7 @@ def get_build_slaves():
     # Builders.
     yield create_slave('lab-mini-02', jobs = 4, max_builds = 1)
     yield create_slave('xserve2', jobs = 4, max_builds = 2)
-    yield create_slave('xserve3', jobs = 4, max_builds = 2)
+    yield create_slave('xserve3', jobs = 2, max_builds = 1)
     yield create_slave('xserve4', jobs = 4, max_builds = 1)
     yield create_slave('xserve5', jobs = 4, max_builds = 1)
 
