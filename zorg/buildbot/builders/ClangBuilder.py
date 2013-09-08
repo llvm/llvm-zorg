e6cafea1f69f441facf957006298f1681a247c2a
Michael Gottesman <mgottesman@apple.com>
2013-09-08 03:04:43 +0000

    [llvmlab] Fix typo caused by my switching from messing with Autotools->CMake->Autotools->CMake->... like a buffalo.
    llvm-svn: 190266

--- zorg/buildbot/builders/ClangBuilder.py
+++ zorg/buildbot/builders/ClangBuilder.py
@@ -799,7 +799,7 @@ def phasedClang(config_options, is_bootstrap=True, use_lto=False,
           '--with-extra-options=-flto -gline-tables-only')
     
     if use_cxx11:
-        configure_args.append("CXX_FLAGS=-std=c++11 -stdlib=libc++ -nostdinc++")
+        configure_args.append("CXXFLAGS=-std=c++11 -stdlib=libc++ -nostdinc++")
 
     # Configure the LLVM build.
     if incremental:
