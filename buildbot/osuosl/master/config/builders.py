47f167302286b9929d9ebb4603d4d3f65a38a1b4
Tobias Grosser <tobias@grosser.es>
2014-03-11 13:43:24 +0000

    pollyperf builder: Use a local package cache
    llvm-svn: 203569

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -606,6 +606,7 @@ def _get_polly_builders():
          'factory': PollyBuilder.getPollyLNTFactory(triple="x86_64-pc-linux-gnu",
                                                     nt_flags=['--multisample=10'],
                                                     reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
                                                     testerName='pollyperf-O3')},
         {'name': "polly-perf-O3-polly",
          'slavenames':["pollyperf6"],
@@ -614,6 +615,7 @@ def _get_polly_builders():
                                                     nt_flags=['--multisample=10', '--mllvm=-polly'],
                                                     xfails=polly_x86_64_linux_xfails,
                                                     reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
                                                     testerName='pollyperf-O3-polly')},
         {'name': "polly-perf-O3-polly-codegen-isl",
          'slavenames':["pollyperf7"],
@@ -622,6 +624,7 @@ def _get_polly_builders():
                                                     nt_flags=['--multisample=10', '--mllvm=-polly', '--mllvm=-polly-code-generator=isl'],
                                                     xfails=polly_x86_64_linux_xfails,
                                                     reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
                                                     testerName='pollyperf-O3-polly-codegen-isl')},
         {'name': "polly-perf-O3-polly-scev",
          'slavenames':["pollyperf10"],
@@ -630,6 +633,7 @@ def _get_polly_builders():
                                                     nt_flags=['--multisample=10', '--mllvm=-polly', '--mllvm=-polly-codegen-scev'],
                                                     xfails=polly_perf_O3_polly_scev_codegen_xfails,
                                                     reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
                                                     testerName='pollyperf-O3-polly-scev')},
         {'name': "polly-perf-O3-polly-scev-codegen-isl",
          'slavenames':["pollyperf11"],
@@ -638,6 +642,7 @@ def _get_polly_builders():
                                                     nt_flags=['--multisample=10', '--mllvm=-polly', '--mllvm=-polly-code-generator=isl', '--mllvm=-polly-codegen-scev'],
                                                     xfails=polly_perf_O3_polly_scev_codegen_xfails,
                                                     reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
                                                     testerName='pollyperf-O3-polly-scev-codegen-isl')},
         {'name': "polly-perf-O3-polly-detect",
          'slavenames':["pollyperf14"],
@@ -646,6 +651,7 @@ def _get_polly_builders():
                                                     nt_flags=['--multisample=10', '--mllvm=-polly', '--mllvm=-polly-code-generator=none', '--mllvm=-polly-optimizer=none'],
                                                     xfails=polly_perf_O3_polly_detect_xfails,
                                                     reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
                                                     testerName='pollyperf-O3-polly-detect')}
        ]
 
