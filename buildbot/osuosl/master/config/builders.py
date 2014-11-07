e776592a1b33cbe1c7adb425c0947eed4b94afc3
Galina Kistanova <gkistanova@gmail.com>
2014-11-07 18:53:46 +0000

    Enabled check-tsan step on FreeBSD sanitizers buildbot.
    llvm-svn: 221538

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -655,7 +655,7 @@ def _get_sanitizer_builders():
            'builddir':"sanitizer_x86_64-freebsd",
            'factory' : SanitizerBuilderII.getSanitizerBuildFactoryII(
                                           clean=True,
-                                          sanitizers=['sanitizer','asan','lsan','ubsan'],
+                                          sanitizers=['sanitizer','asan','lsan','tsan','ubsan'],
                                           common_cmake_options='-DCMAKE_EXE_LINKER_FLAGS=-lcxxrt')},
 
           {'name': "sanitizer-ppc64-linux1",
