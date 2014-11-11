189eb9b03385051b7f815be71f77ec8bf80363e6
Eric Fiselier <eric@efcs.ca>
2014-11-11 06:44:50 +0000

    Add OSX buildslave
    llvm-svn: 221670

--- buildbot/osuosl/master/config/slaves.py
+++ buildbot/osuosl/master/config/slaves.py
@@ -160,7 +160,8 @@ def get_build_slaves():
 
         # Ubuntu 14.04 x86_64, Intel(R) Xeon(R) CPU E5-1650 0 @ 3.20GHz
         create_slave("ericwf-buildslave", properties={'jobs': 4}, max_builds=2),
-
+        # OS X 10.10 x86_64, Intel Core 2 Duo @ 2.40GHz
+        create_slave("ericwf-osx-slave", properties={'jobs': 2}, max_build=1),
         # Defunct.
 
 #        # GCC Compile Farm Slaves, see http://gcc.gnu.org/wiki/CompileFarm
