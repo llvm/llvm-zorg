1a6699f75fa4553d5115a6ec8226e44a23ae7cf9
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2014-12-04 14:03:18 +0000

    Sanitizer bot: build stage1 clang with debug info.
    The host compiler does not always support -gmlt.
    
    llvm-svn: 223362

--- zorg/buildbot/builders/sanitizers/buildbot_cmake.sh
+++ zorg/buildbot/builders/sanitizers/buildbot_cmake.sh
@@ -59,8 +59,8 @@ echo @@@BUILD_STEP build fresh clang@@@
 if [ ! -d clang_build ]; then
   mkdir clang_build
 fi
-(cd clang_build && cmake -DCMAKE_BUILD_TYPE=Release \
-    ${CMAKE_COMMON_OPTIONS} -DCMAKE_C_FLAGS=-gmlt -DCMAKE_CXX_FLAGS=-gmlt $LLVM_CHECKOUT)
+(cd clang_build && cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    ${CMAKE_COMMON_OPTIONS} $LLVM_CHECKOUT)
 (cd clang_build && make clang -j$MAKE_JOBS) || echo @@@STEP_FAILURE@@@
 
 # If we're building with libcxx, install the headers to clang_build/include.
