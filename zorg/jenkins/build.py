5bd09984f7a7432fc152668da7ad05853b0544a9
Chris Matthews <cmatthews5@apple.com>
2014-12-06 00:01:10 +0000

    Make configure tests more verbose in Jenkins
    llvm-svn: 223545

--- zorg/jenkins/build.py
+++ zorg/jenkins/build.py
@@ -157,7 +157,7 @@ def clang_builder(target):
 
     make_install = ["make", "install-clang", "-j", conf.j_level]
 
-    make_check = ["make", "VERBOSE=1", "check-all", 'LIT_ARGS="--xunit-xml-output=testresults.xunit.xml"']
+    make_check = ["make", "VERBOSE=1", "check-all", 'LIT_ARGS=--xunit-xml-output=testresults.xunit.xml -v']
 
     if target == 'build' or target == 'all':
         header("Configure")
