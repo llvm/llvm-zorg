82c088c296b8566817aaaa8e02d53c58f0529829
Sergey Matveev <earthdok@google.com>
2015-01-31 12:35:16 +0000

    Add slave sanitizer-buildbot5.
    Also adjust the number of jobs on other sanitizer slaves.
    
    llvm-svn: 227687

--- buildbot/osuosl/master/config/slaves.py
+++ buildbot/osuosl/master/config/slaves.py
@@ -160,13 +160,15 @@ def get_build_slaves():
         create_slave("juno-aarch64-01", properties={'jobs': 4}, max_builds=1),
 
         # Debian 7.7 x86_64 16-core GCE instance
-        create_slave("sanitizer-buildbot1", properties={'jobs': 8}, max_builds=1),
+        create_slave("sanitizer-buildbot1", properties={'jobs': 16}, max_builds=1),
         # Debian 7.7 x86_64 16-core GCE instance
-        create_slave("sanitizer-buildbot2", properties={'jobs': 8}, max_builds=1),
+        create_slave("sanitizer-buildbot2", properties={'jobs': 16}, max_builds=1),
         # Debian 7.7 x86_64 8-core GCE instance
         create_slave("sanitizer-buildbot3", properties={'jobs': 8}, max_builds=1),
         # Debian 7.7 x86_64 8-core GCE instance
         create_slave("sanitizer-buildbot4", properties={'jobs': 8}, max_builds=1),
+        # Debian 7.7 x86_64 8-core GCE instance
+        create_slave("sanitizer-buildbot5", properties={'jobs': 8}, max_builds=1),
 
         # zEnterprise 196 (s390x), SLES 11 SP2
         create_slave("systemz-1", properties={'jobs': 4}, max_builds=1),
