6bf0609f6ae8a8811729e41694f614d0f77e828a
Galina Kistanova <gkistanova@gmail.com>
2015-02-17 22:05:25 +0000

    Reverted r229559 since 3 arguments required.
    llvm-svn: 229565

--- buildbot/osuosl/master/config/status.py
+++ buildbot/osuosl/master/config/status.py
@@ -46,8 +46,10 @@ def get_status_targets(standard_builders):
             http_port = 8011, authz=authz_cfg),
         buildbot.status.mail.MailNotifier(
             fromaddr = "llvm.buildmaster@lab.llvm.org",
+            extraRecipients = [default_email],
             lookup = ConfigEmailLookup(os.path.join(os.path.dirname(__file__),
-                                                    "llvmauthors.cfg")),
+                                                    "llvmauthors.cfg"),
+                                       default_email),
             mode = "problem",
             builders = standard_builders),
         buildbot.status.words.IRC(
