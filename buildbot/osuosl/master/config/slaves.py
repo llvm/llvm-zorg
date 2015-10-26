f0f67c7be0a45f8a50cf9283ca34eee9e7454921
Galina Kistanova <gkistanova@gmail.com>
2015-10-26 17:18:45 +0000

    Increased jobs number for slave ps4-buildslave1. Patch by Michael Edwards.
    llvm-svn: 251311

--- buildbot/osuosl/master/config/slaves.py
+++ buildbot/osuosl/master/config/slaves.py
@@ -193,7 +193,7 @@ def get_build_slaves():
         create_slave("bpf-build-slave01", properties={'jobs': 16}, max_builds=1),
 
         # Ubuntu 14.04 x86_64-scei-ps4, 2 x Intel(R) Xeon(R) CPU E5-2699 v3 @ 2.30GHz
-        create_slave("ps4-buildslave1", properties={'jobs': 64}, max_builds=1),
+        create_slave("ps4-buildslave1", properties={'jobs': 72}, max_builds=1),
 
         # NetBSD 7.0 amd64
         create_slave("lldb-amd64-ninja-netbsd7", properties={'jobs': 3}, max_builds=1),
