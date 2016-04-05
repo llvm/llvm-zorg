197cb75b411736e27fe8e772da61da681c09d81f
Galina Kistanova <gkistanova@gmail.com>
2016-04-05 21:27:32 +0000

    Removed reloads from builders.py, as they do not work as the author expected anyway.​
    llvm-svn: 265472

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -1,68 +1,20 @@
 from zorg.buildbot.builders import ClangBuilder
-reload(ClangBuilder)
-from zorg.buildbot.builders import ClangBuilder
-
 from zorg.buildbot.builders import LLVMBuilder
-reload(LLVMBuilder)
-from zorg.buildbot.builders import LLVMBuilder
-
 from zorg.buildbot.builders import LNTBuilder
-reload(LNTBuilder)
-from zorg.buildbot.builders import LNTBuilder
-
 from zorg.buildbot.builders import NightlytestBuilder
-reload(NightlytestBuilder)
-from zorg.buildbot.builders import NightlytestBuilder
-
 from zorg.buildbot.builders import PollyBuilder
-reload(PollyBuilder)
-from zorg.buildbot.builders import PollyBuilder
-
-from zorg.buildbot.builders import LLDBBuilder
-reload(LLDBBuilder)
 from zorg.buildbot.builders import LLDBBuilder
 from zorg.buildbot.builders.LLDBBuilder import RemoteConfig
-
 from zorg.buildbot.builders import LLDBuilder
-reload(LLDBuilder)
-from zorg.buildbot.builders import LLDBuilder
-
 from zorg.buildbot.builders import LLGoBuilder
-reload(LLGoBuilder)
-from zorg.buildbot.builders import LLGoBuilder
-
 from zorg.buildbot.builders import ClangAndLLDBuilder
-reload(ClangAndLLDBuilder)
-from zorg.buildbot.builders import ClangAndLLDBuilder
-
 from zorg.buildbot.builders import SanitizerBuilder
-reload(SanitizerBuilder)
-from zorg.buildbot.builders import SanitizerBuilder
-
 from zorg.buildbot.builders import SanitizerBuilderII
-reload(SanitizerBuilderII)
-from zorg.buildbot.builders import SanitizerBuilderII
-
 from zorg.buildbot.builders import SanitizerBuilderWindows
-reload(SanitizerBuilderWindows)
-from zorg.buildbot.builders import SanitizerBuilderWindows
-
 from zorg.buildbot.builders import Libiomp5Builder
-reload(Libiomp5Builder)
-from zorg.buildbot.builders import Libiomp5Builder
-
 from zorg.buildbot.builders import LibcxxAndAbiBuilder
-reload(LibcxxAndAbiBuilder)
-from zorg.buildbot.builders import LibcxxAndAbiBuilder
-
 from zorg.buildbot.builders import SphinxDocsBuilder
-reload(SphinxDocsBuilder)
-from zorg.buildbot.builders import SphinxDocsBuilder
-
 from zorg.buildbot.builders import ABITestsuitBuilder
-reload(ABITestsuitBuilder)
-from zorg.buildbot.builders import ABITestsuitBuilder
-
 from zorg.buildbot.builders import ClangLTOBuilder3Stage
 
 # Plain LLVM builders.
