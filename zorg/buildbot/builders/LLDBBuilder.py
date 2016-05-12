abab9034c125e6e243b5f9034d344b3d0b127c22
Zachary Turner <zturner@google.com>
2016-05-12 18:01:28 +0000

    Fix build directory for LLDB Builder with tests.
    llvm-svn: 269318

--- zorg/buildbot/builders/LLDBBuilder.py
+++ zorg/buildbot/builders/LLDBBuilder.py
@@ -93,7 +93,7 @@ def getLLDBWindowsCMakeBuildFactory(
 
     # Use batch files instead of ShellCommand directly, Windows quoting is
     # borked. FIXME: See buildbot ticket #595 and buildbot ticket #377.
-    clang_exe = 'build_dir/bin/clang.exe'    # ./bin/clang.exe
+    clang_exe = 'bin/clang.exe'    # ./bin/clang.exe
     f.addStep(batch_file_download.BatchFileDownload(name='cmakegen',
                                 command=[cmake, "-G", "Ninja", "../llvm",
                                          "-DCMAKE_BUILD_TYPE="+config,
