c194c38dae916583d14a835516803c17db45ed80
Zachary Turner <zturner@google.com>
2016-05-17 19:50:47 +0000

    Re-enable tests on the LLDB buildbot.
    Manually confirmed that "ninja check-lldb" is successful on
    the buildbot, so crossing my fingers that we're good to go
    this time.
    
    llvm-svn: 269810

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -565,7 +565,7 @@ def _get_lldb_builders():
         {'name': "lldb-x86-windows-msvc2015",
          'slavenames': ["zturner-win2008"],
          'builddir': "lldb-windows-x86",
-         'factory': LLDBBuilder.getLLDBWindowsCMakeBuildFactory()},
+         'factory': LLDBBuilder.getLLDBWindowsCMakeBuildFactory(test=True)},
         # Disable the builder till we fix the cmake configuration
         #{'name': "lldb-x86-win7-msvc",
         #'slavenames': ["hexagon-build-01"],
