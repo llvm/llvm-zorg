d166377ddc6f0763a11ba2b33c9a30c1430f798e
Galina Kistanova <gkistanova@gmail.com>
2016-10-07 20:45:49 +0000

    Refactored to support extra options.
    llvm-svn: 283595

--- zorg/buildbot/commands/CmakeCommand.py
+++ zorg/buildbot/commands/CmakeCommand.py
@@ -5,6 +5,25 @@ from zorg.buildbot.util.helpers import stripQuotationMarks
 class CmakeCommand(WarningCountingShellCommand):
 
     @staticmethod
+    def sanitize_kwargs(kwargs):
+        # kwargs we could get and must not pass through
+        # to the buildstep.RemoteShellCommand constructor.
+        # Note: This is a workaround of the buildbot design issue,
+        # thus should be removed once the original issue gets fixed.
+        consume_kwargs = [
+                             "jobs",
+                             "loadaverage",
+                         ]
+
+        sanitized_kwargs = kwargs.copy()
+        for k in consume_kwargs:
+            if k in sanitized_kwargs.keys():
+                del sanitized_kwargs[k]
+
+        return sanitized_kwargs
+
+
+    @staticmethod
     def applyRequiredOptions(options, required):
         # required is a list of tuples in form of (<opt name>, <opt value>),
         # where all the values are properly formatted to terminate
@@ -129,11 +148,15 @@ class CmakeCommand(WarningCountingShellCommand):
         if self.path:
             command += self.path
 
-        # Note: We will remove all the empty items from the command at start.
-        kwargs['command'] = command
+        # Remove here all the kwargs any of our LLVM buildbot command could consume.
+        # Note: We will remove all the empty items from the command at start, as we
+        # still didn't get yet WithProperties rendered.
+        sanitized_kwargs = self.sanitize_kwargs(kwargs)
+
+        sanitized_kwargs["command"] = command
 
         # And upcall to let the base class do its work
-        WarningCountingShellCommand.__init__(self, **kwargs)
+        WarningCountingShellCommand.__init__(self, **sanitized_kwargs)
 
         self.addFactoryArguments(prefixCommand=prefixCommand,
                                  options=self.options,
