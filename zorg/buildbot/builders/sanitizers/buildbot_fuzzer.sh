ab32d9392333ed34a2d3f2d6a173c17c75e44235
Kostya Serebryany <kcc@google.com>
2017-08-04 19:30:40 +0000

    [libFuzzer] add -DLIBFUZZER_ENABLE_TESTS=ON to libFuzzer bot
    llvm-svn: 310102

--- zorg/buildbot/builders/sanitizers/buildbot_fuzzer.sh
+++ zorg/buildbot/builders/sanitizers/buildbot_fuzzer.sh
@@ -19,7 +19,7 @@ LLVM=$ROOT/llvm
 LIBFUZZER=$LLVM/lib/Fuzzer
 # No assertions. Need to clean up the existing assertion failures first.
 # Also, the Fuzzer does not provide reproducers on assertion failures yet.
-CMAKE_COMMON_OPTIONS="-GNinja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_PARALLEL_LINK_JOBS=8 -DLLVM_APPEND_VC_REV=OFF"
+CMAKE_COMMON_OPTIONS="-GNinja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=OFF -DLLVM_PARALLEL_LINK_JOBS=8 -DLLVM_APPEND_VC_REV=OFF -DLIBFUZZER_ENABLE_TESTS=ON"
 
 if [ "$BUILDBOT_CLOBBER" != "" ]; then
   echo @@@BUILD_STEP clobber@@@
