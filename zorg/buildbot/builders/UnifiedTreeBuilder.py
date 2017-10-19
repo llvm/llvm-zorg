ffc4c61d56250351a8b442e57851adbd2b13f8ab
Galina Kistanova <gkistanova@gmail.com>
2017-10-19 18:55:33 +0000

    Fixed typo.
    llvm-svn: 316175

--- zorg/buildbot/builders/UnifiedTreeBuilder.py
+++ zorg/buildbot/builders/UnifiedTreeBuilder.py
@@ -124,7 +124,7 @@ def addCmakeSteps(
                            haltOnFailure=kwargs.get('haltOnFailure', True),
                            env=env,
                            workdir=obj_dir,
-                           doStepIf=FileDoesNotExist("%s/CMakeCache.txt" % objdir),
+                           doStepIf=FileDoesNotExist("%s/CMakeCache.txt" % obj_dir),
                            **kwargs # Pass through all the extra arguments.
                            ))
 
