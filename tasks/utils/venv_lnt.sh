cf5cb05e9b88d359026d75e3d7a51b1a4b60b9a1
Matthias Braun <matze@braunis.de>
2017-11-07 19:06:58 +0000

    jenkins/tasks: Simplify lnt installation
    llvm-svn: 317602

--- tasks/utils/venv_lnt.sh
+++ tasks/utils/venv_lnt.sh
@@ -1,7 +1,3 @@
 echo "@@@ Install VirtualEnv LNT @@@"
-# It seems that lnt/setup.py does not utilize the python/wheel package caches.
-# Not sure why, we work around the problem by installing the requirements
-# first:
-. "${TASKDIR}"/utils/pip_install.sh -r lnt/requirements.client.txt
-python lnt/setup.py develop
+. "${TASKDIR}"/utils/pip_install.sh lnt/
 echo "@@@@@@"
