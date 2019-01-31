c52e5c5f082d6ba7282fe6aa82dfdfc3422b4cf1
Galina Kistanova <gkistanova@gmail.com>
2019-01-31 05:34:32 +0000

    Build llvm-clang-lld-x86_64-scei-ps4-windows10pro-fast and llvm-clang-x86_64-expensive-checks-win with the latest installed version of Visual Studio.
    llvm-svn: 352727

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -160,6 +160,7 @@ def _get_clang_fast_builders():
                                         "-DLLVM_TARGETS_TO_BUILD=X86"],
                      triple="x86_64-scei-ps4",
                      isMSVC=True,
+                     vs="autodetect",
                      prefixCommand=None, # This is a designated builder, so no need to be nice.
                      extraLitArgs=["-j80"])},
 
@@ -167,10 +168,11 @@ def _get_clang_fast_builders():
          'slavenames':["ps4-buildslave2"],
          'builddir':"llvm-clang-x86_64-expensive-checks-win",
          'factory': UnifiedTreeBuilder.getCmakeWithNinjaWithMSVCBuildFactory(
-                extra_configure_args = ["-DLLVM_ENABLE_EXPENSIVE_CHECKS=ON",
-                                        "-DLLVM_ENABLE_WERROR=OFF",
-                                        "-DCMAKE_BUILD_TYPE=Debug",
-                                        "-DLLVM_LIT_ARGS='-v'"])},
+                      vs="autodetect",
+                      extra_configure_args=["-DLLVM_ENABLE_EXPENSIVE_CHECKS=ON",
+                                            "-DLLVM_ENABLE_WERROR=OFF",
+                                            "-DCMAKE_BUILD_TYPE=Debug",
+                                            "-DLLVM_LIT_ARGS='-v'"])},
     ]
 
 # Clang builders.
