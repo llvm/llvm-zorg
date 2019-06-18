c116b7864adf13cfd4c44e918ec00a0cee307b10
Omair Javaid <omair.javaid@linaro.org>
2019-06-18 11:01:33 +0000

    Add lit timeout for lldb arm/aarch64 ubuntu builders
    This patch adds a timeout interval of 200 seconds for any hanging tests.
    
    llvm-svn: 363660

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -852,7 +852,7 @@ def _get_lldb_builders():
          'factory': LLDBBuilder.getLLDBCMakeBuildFactory(
                     test=True,
                     extra_cmake_args=['-DLLVM_ENABLE_ASSERTIONS=True',
-                                      '-DLLVM_LIT_ARGS="-sv --threads=8"',
+                                      '-DLLVM_LIT_ARGS="-sv --threads=8 --timeout=200"',
                                       '-DLLVM_USE_LINKER=gold',
                                       '-DCMAKE_C_COMPILER=clang',
                                       '-DCMAKE_CXX_COMPILER=clang++'])},
@@ -863,7 +863,7 @@ def _get_lldb_builders():
          'factory': LLDBBuilder.getLLDBCMakeBuildFactory(
                     test=True,
                     extra_cmake_args=['-DLLVM_ENABLE_ASSERTIONS=True',
-                                      '-DLLVM_LIT_ARGS="-sv --threads=8"',
+                                      '-DLLVM_LIT_ARGS="-sv --threads=8 --timeout=200"',
                                       '-DLLVM_USE_LINKER=gold',
                                       '-DCMAKE_C_COMPILER=clang',
                                       '-DCMAKE_CXX_COMPILER=clang++'])},
