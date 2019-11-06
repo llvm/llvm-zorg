llvm/llvm-zorg

commit 4f54ad8e5193dfd4e1acae49a3285c6b6a9f2d66

Add an override for ignoring errors

zorg/jenkins/monorepo_build.py
@@ -598,7 +598,7 @@ def lldb_cmake_builder(target, variant=None):
 
     for test_target in conf.cmake_test_targets:
         header("Run Custom Test: {0}".format(test_target))
-        run_cmd(conf.lldbbuilddir(), [NINJA, '-k', '0', '-v', test_target], err_okay=True)
+        run_cmd(conf.lldbbuilddir(), [NINJA, '-k', '0', '-v', test_target])
         footer()
 
 
@@ -847,13 +847,14 @@ def run_cmd(working_dir, cmd, env=None, sudo=False, err_okay=False):
     sys.stdout.flush()
     return_code = 0
     start_time = datetime.datetime.now()
+    ignore_errors_override = os.environ.get('IGNORE_ERRORS_OVERRIDE', False)
     if not os.environ.get('TESTING', False):
         try:
             os.chdir(working_dir)
             subprocess.check_call(cmd)
             os.chdir(old_cwd)
         except subprocess.CalledProcessError as excpt:
-            if not err_okay:
+            if not err_okay and not ignore_errors_override::
                 raise excpt
             else:
                 logging.info("Ignoring failed command.")

commit 070a8f8889058ad5808921813a3ce32f35350f55

Fix syntax error introduced in 4f54ad8e

zorg/jenkins/monorepo_build.py
@@ -854,7 +854,7 @@ def run_cmd(working_dir, cmd, env=None, sudo=False, err_okay=False):
             subprocess.check_call(cmd)
             os.chdir(old_cwd)
         except subprocess.CalledProcessError as excpt:
-            if not err_okay and not ignore_errors_override::
+            if not err_okay and not ignore_errors_override:
                 raise excpt
             else:
                 logging.info("Ignoring failed command.")