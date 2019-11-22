16c86f773c0842997336f431fce4040d457caa1f
Galina Kistanova <gkistanova@gmail.com>
2019-11-21 23:03:03 -0800

    Added the first github status reporter in pilot mode.

--- buildbot/osuosl/master/config/status.py
+++ buildbot/osuosl/master/config/status.py
@@ -7,6 +7,7 @@ import buildbot.status.words
 import config
 from zorg.buildbot.util.ConfigEmailLookup import ConfigEmailLookup
 from zorg.buildbot.util.InformativeMailNotifier import InformativeMailNotifier
+from zorg.buildbot.status.github import GitHubStatus
 
 # Returns a list of Status Targets. The results of each build will be
 # pushed to these targets. buildbot/status/*.py has a variety to choose from,
@@ -29,11 +30,20 @@ def get_status_targets(standard_builders, standard_categories=None):
                       )
 
     default_email = config.options.get('Master Options', 'default_email')
+    github_token = config.options.get('GitHub Status', 'token')
 
     return [
         buildbot.status.html.WebStatus(
             order_console_by_time=True,
             http_port = 8011, authz=authz_cfg),
+
+        GitHubStatus(
+            token=github_token,
+            repoOwner='llvm',
+            repoName='llvm-project',
+            builders_to_report = [
+                "llvm-clang-lld-x86_64-scei-ps4-ubuntu-fast",
+            ]),
 
         # All the standard builders send e-mail and IRC notifications.
         buildbot.status.mail.MailNotifier(
