ab4824235f7b43bc0c23d5cc077f1fd8d240c4fb
Dmitri Gribenko <gribozavr@gmail.com>
2019-12-03 13:21:08 +0100

    Removed legacy mode from OpenMPBuilder

--- zorg/buildbot/builders/OpenMPBuilder.py
+++ zorg/buildbot/builders/OpenMPBuilder.py
@@ -21,7 +21,6 @@ def getOpenMPCMakeBuildFactory(
         ompt                = False,        # Whether to enable the OpenMP Tools Interface.
         test                = True,         # Test the built libraries.
         depends_on_projects = None,
-        is_legacy_mode      = False,
         **kwargs):
 
     # Prepare environmental variables. Set here all env we want everywhere.
@@ -38,203 +37,82 @@ def getOpenMPCMakeBuildFactory(
 
     cleanBuildRequested = lambda step: clean or step.build.getProperty("clean", default=step.build.getProperty("clean_obj"))
 
-    if is_legacy_mode:
+    if depends_on_projects is None:
+        # Monorepo configuration requires llvm and clang to get cmake work.
+        depends_on_projects = ['llvm', 'clang', 'openmp']
 
-        openmp_srcdir = 'openmp.src'
-        openmp_builddir = 'openmp.build'
+    f = UnifiedTreeBuilder.getLLVMBuildFactoryAndSourcecodeSteps(
+            depends_on_projects=depends_on_projects,
+            llvm_srcdir=llvm_srcdir,
+            obj_dir=llvm_builddir,
+            cleanBuildRequested=cleanBuildRequested,
+            env=merged_env,
+            **kwargs) # Pass through all the extra arguments.
 
-        f = buildbot.process.factory.BuildFactory()
+    f.addStep(
+        ShellCommand(
+            name            = 'clean',
+            command         = ['rm', '-rf', f.obj_dir],
+            warnOnFailure   = True,
+            description     = ['clean'],
+            doStepIf        = cleanBuildRequested,
+            workdir         = '.',
+            env             = merged_env))
 
-        # Checkout sources.
-        f.addStep(
-            SVN(
-                name            = 'svn-openmp',
-                mode            = 'update',
-                baseURL         = 'http://llvm.org/svn/llvm-project/openmp/',
-                defaultBranch   = 'trunk',
-                workdir         = openmp_srcdir))
+    # Configure LLVM and OpenMP (and Clang, if requested).
+    cmake_args  = ['cmake', '-G', 'Ninja']
+    cmake_args += ['-DCMAKE_BUILD_TYPE=Release', '-DLLVM_ENABLE_ASSERTIONS=ON']
+    cmake_args += ['-DCMAKE_C_COMPILER=%s' % c_compiler]
+    cmake_args += ['-DCMAKE_CXX_COMPILER=%s' % cxx_compiler]
+    if ompt:
+        cmake_args += ['-DLIBOMP_OMPT_SUPPORT=ON']
+    if test:
+        lit_args = '-vv --show-unsupported --show-xfail -j %s' % jobs
+        cmake_args += [WithProperties('-DLLVM_LIT_ARGS=%s' % lit_args)]
 
-        # LLVM sources are needed to build utilities for testing.
-        f.addStep(
-            SVN(
-                name            = 'svn-llvm',
-                mode            = 'update',
-                baseURL         = 'http://llvm.org/svn/llvm-project/llvm/',
-                defaultBranch   = 'trunk',
-                workdir         = llvm_srcdir))
+    CmakeCommand.applyRequiredOptions(cmake_args, [
+        ('-DLLVM_ENABLE_PROJECTS=', ";".join(f.depends_on_projects)),
+        ])
 
-        # Get Clang sources, if requested.
-        if build_clang:
-            f.addStep(
-                SVN(
-                    name            = 'svn-clang',
-                    mode            = 'update',
-                    baseURL         = 'http://llvm.org/svn/llvm-project/cfe/',
-                    defaultBranch   = 'trunk',
-                    workdir         = '%s/tools/clang' % llvm_srcdir))
+    # Add llvm-lit and clang (if built) to PATH
+    merged_env.update({
+        'PATH': WithProperties('%(workdir)s/' + llvm_builddir + '/bin:${PATH}')})
 
-        f.addStep(
-            ShellCommand(
-                name            = 'clean',
-                command         = ['rm', '-rf', openmp_builddir, llvm_builddir],
-                warnOnFailure   = True,
-                description     = ['clean'],
-                doStepIf        = cleanBuildRequested,
-                workdir         = '.',
-                env             = merged_env))
+    src_dir = LLVMBuildFactory.pathRelativeToBuild(f.llvm_srcdir, f.obj_dir)
 
-        # Configure LLVM (and Clang, if requested).
-        cmake_args  = ['cmake', '-G', 'Ninja', '../%s' % llvm_srcdir]
-        cmake_args += ['-DCMAKE_BUILD_TYPE=Release', '-DLLVM_ENABLE_ASSERTIONS=ON']
-        cmake_args += ['-DCMAKE_C_COMPILER=%s' % c_compiler]
-        cmake_args += ['-DCMAKE_CXX_COMPILER=%s' % cxx_compiler]
-        f.addStep(
-            Configure(
-                name        = 'configure-llvm',
-                command     = cmake_args,
-                description = 'configure llvm',
-                workdir     = llvm_builddir,
-                env         = merged_env))
+    f.addStep(CmakeCommand(name='configure-openmp',
+                           description=['configure','openmp'],
+                           options=cmake_args,
+                           path=src_dir,
+                           env=merged_env,
+                           workdir=f.obj_dir,
+                           haltOnFailure=True,
+                           **kwargs # Pass through all the extra arguments.
+                           ))
 
-        # Build LLVM utils.
-        f.addStep(
-            NinjaCommand(
-                name        = 'compile-llvm-utils',
-                targets     = ['FileCheck'],
-                description = 'compile llvm utils',
-                workdir     = llvm_builddir,
-                env         = merged_env))
+    # Build OpenMP runtime libraries.
+    f.addStep(
+        NinjaCommand(
+            name        = 'compile-openmp',
+            description = 'compile openmp',
+            workdir     = f.obj_dir,
+            env         = merged_env,
+            haltOnFailure=True))
 
-        # Build Clang if requested.
-        if build_clang:
-            f.addStep(
-                NinjaCommand(
-                    name        = 'compile-clang',
-                    description = 'compile clang',
-                    workdir     = llvm_builddir,
-                    env         = merged_env))
-
+    # Test OpenMP runtime libraries, if requested.
+    if test:
         # Add llvm-lit and clang (if built) to PATH
         merged_env.update({
             'PATH': WithProperties('%(workdir)s/' + llvm_builddir + '/bin:${PATH}')})
 
-        # Configure OpenMP runtime libraries.
-        cmake_args  = ['cmake', '-G', 'Ninja', '../%s' % openmp_srcdir]
-        cmake_args += ['-DCMAKE_C_COMPILER=%s' % c_compiler]
-        cmake_args += ['-DCMAKE_CXX_COMPILER=%s' % cxx_compiler]
-        if ompt:
-            cmake_args += ['-DLIBOMP_OMPT_SUPPORT=ON']
-
-        if test:
-            lit_args = '-v --show-unsupported --show-xfail -j %s' % jobs
-            cmake_args += [WithProperties('-DOPENMP_LIT_ARGS=%s' % lit_args)]
-
+        ninja_test_args = ['ninja', WithProperties('-j %s' % jobs)]
         f.addStep(
-            Configure(
-                name        = 'configure-openmp',
-                command     = cmake_args,
-                description = 'configure openmp',
-                workdir     = openmp_builddir,
-                env         = merged_env))
-
-        # Build OpenMP runtime libraries.
-        f.addStep(
-            NinjaCommand(
-                name        = 'compile-openmp',
-                description = 'compile openmp',
-                workdir     = openmp_builddir,
-                env         = merged_env))
-
-        # Test OpenMP runtime libraries, if requested.
-        if test:
-            ninja_test_args = ['ninja', WithProperties('-j %s' % jobs)]
-            f.addStep(
-                LitTestCommand(
-                    name        = 'test-openmp',
-                    command     = ninja_test_args + ['check-openmp'],
-                    description = 'test openmp',
-                    workdir     = openmp_builddir,
-                    env         = merged_env))
-
-    else:
-
-        if depends_on_projects is None:
-            # Monorepo configuration requires llvm and clang to get cmake work.
-            depends_on_projects = ['llvm', 'clang', 'openmp']
-
-        f = UnifiedTreeBuilder.getLLVMBuildFactoryAndSourcecodeSteps(
-                is_legacy_mode=is_legacy_mode,
-                depends_on_projects=depends_on_projects,
-                llvm_srcdir=llvm_srcdir,
-                obj_dir=llvm_builddir,
-                cleanBuildRequested=cleanBuildRequested,
-                env=merged_env,
-                **kwargs) # Pass through all the extra arguments.
-
-        f.addStep(
-            ShellCommand(
-                name            = 'clean',
-                command         = ['rm', '-rf', f.obj_dir],
-                warnOnFailure   = True,
-                description     = ['clean'],
-                doStepIf        = cleanBuildRequested,
-                workdir         = '.',
-                env             = merged_env))
-
-        # Configure LLVM and OpenMP (and Clang, if requested).
-        cmake_args  = ['cmake', '-G', 'Ninja']
-        cmake_args += ['-DCMAKE_BUILD_TYPE=Release', '-DLLVM_ENABLE_ASSERTIONS=ON']
-        cmake_args += ['-DCMAKE_C_COMPILER=%s' % c_compiler]
-        cmake_args += ['-DCMAKE_CXX_COMPILER=%s' % cxx_compiler]
-        if ompt:
-            cmake_args += ['-DLIBOMP_OMPT_SUPPORT=ON']
-        if test:
-            lit_args = '-vv --show-unsupported --show-xfail -j %s' % jobs
-            cmake_args += [WithProperties('-DLLVM_LIT_ARGS=%s' % lit_args)]
-
-        CmakeCommand.applyRequiredOptions(cmake_args, [
-            ('-DLLVM_ENABLE_PROJECTS=', ";".join(f.depends_on_projects)),
-            ])
-
-        # Add llvm-lit and clang (if built) to PATH
-        merged_env.update({
-            'PATH': WithProperties('%(workdir)s/' + llvm_builddir + '/bin:${PATH}')})
-
-        src_dir = LLVMBuildFactory.pathRelativeToBuild(f.llvm_srcdir, f.obj_dir)
-
-        f.addStep(CmakeCommand(name='configure-openmp',
-                               description=['configure','openmp'],
-                               options=cmake_args,
-                               path=src_dir,
-                               env=merged_env,
-                               workdir=f.obj_dir,
-                               haltOnFailure=True,
-                               **kwargs # Pass through all the extra arguments.
-                               ))
-
-        # Build OpenMP runtime libraries.
-        f.addStep(
-            NinjaCommand(
-                name        = 'compile-openmp',
-                description = 'compile openmp',
+            LitTestCommand(
+                name        = 'test-openmp',
+                command     = ninja_test_args + ['check-openmp'],
+                description = 'test openmp',
                 workdir     = f.obj_dir,
                 env         = merged_env,
                 haltOnFailure=True))
-
-        # Test OpenMP runtime libraries, if requested.
-        if test:
-            # Add llvm-lit and clang (if built) to PATH
-            merged_env.update({
-                'PATH': WithProperties('%(workdir)s/' + llvm_builddir + '/bin:${PATH}')})
-
-            ninja_test_args = ['ninja', WithProperties('-j %s' % jobs)]
-            f.addStep(
-                LitTestCommand(
-                    name        = 'test-openmp',
-                    command     = ninja_test_args + ['check-openmp'],
-                    description = 'test openmp',
-                    workdir     = f.obj_dir,
-                    env         = merged_env,
-                    haltOnFailure=True))
 
     return f
