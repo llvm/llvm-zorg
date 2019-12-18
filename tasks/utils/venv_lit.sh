70fd0a372df64319d5ab4ba5126ba355e2048fa5
Jessica Paquette <jpaquette@apple.com>
2019-12-18 13:27:43 -0800

    [tasktool] Try to make pip happy with installing lit
    Add github as a trusted host when installing lit from there.

--- tasks/utils/venv_lit.sh
+++ tasks/utils/venv_lit.sh
@@ -1,3 +1,3 @@
 echo "@@@ Install VirtualEnv Lit @@@"
-. "${TASKDIR}"/utils/pip_install.sh svn+https://github.com/llvm/llvm-project/trunk/llvm/utils/lit/
+. "${TASKDIR}"/utils/pip_install.sh --trusted-host github.com svn+https://github.com/llvm/llvm-project/trunk/llvm/utils/lit/
 echo "@@@@@@"
