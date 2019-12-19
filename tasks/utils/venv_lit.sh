5d9ac9c5e2a6691dc91fc5883ffd5d7a201197db
Jessica Paquette <jpaquette@apple.com>
2019-12-19 13:33:38 -0800

    [tasktool] Install default lit from pip
    For some reason, even though lit is *installed*, it doesn't actually work when
    you install the latest version via pip.
    
    For the sake of keeping any LNT-based bots green, let's just use the lit that
    you can get off pip. We were using a stale version for months anyway, so
    it's not like we're making anything objectively worse.

--- tasks/utils/venv_lit.sh
+++ tasks/utils/venv_lit.sh
@@ -1,4 +1,3 @@
 echo "@@@ Install VirtualEnv Lit @@@"
-svn checkout -q --trust-server-cert-failures=unknown-ca --non-interactive https://github.com/llvm/llvm-project/trunk/llvm/utils/lit/ lit-src
-. "${TASKDIR}"/utils/pip_install.sh lit-src/
+. "${TASKDIR}"/utils/pip_install.sh lit
 echo "@@@@@@"
