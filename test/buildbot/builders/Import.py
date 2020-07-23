4c22206f091bc7ed56aeedbada5d703620655780
Galina Kistanova <gkistanova@gmail.com>
2020-07-23 14:59:17 -0700

    Removed deprecated NightlytestBuilder.

--- test/buildbot/builders/Import.py
+++ test/buildbot/builders/Import.py
@@ -2,7 +2,6 @@
 
 import zorg
 from zorg.buildbot.builders import ClangBuilder, LLVMBuilder, LLVMGCCBuilder
-from zorg.buildbot.builders import NightlytestBuilder
 from zorg.buildbot.builders import SanitizerBuilder
 
 # Just check that we can instantiate the build factors, what else can we do?
@@ -12,7 +11,5 @@ print ClangBuilder.getClangBuildFactory()
 print LLVMBuilder.getLLVMBuildFactory()
 
 print LLVMGCCBuilder.getLLVMGCCBuildFactory()
-
-print NightlytestBuilder.getFastNightlyTestBuildFactory('x86_64-apple-darwin10')
 
 print SanitizerBuilder.getSanitizerBuildFactory()
