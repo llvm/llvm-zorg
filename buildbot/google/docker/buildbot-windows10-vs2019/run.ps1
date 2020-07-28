f5229f92248f3bea00bd3de2e0a57d3124370859
Christian Kühnel <kuhnel@google.com>
2020-07-28 13:37:35 +0000

    moved buildbot working dir to volume

--- buildbot/google/docker/buildbot-windows10-vs2019/run.ps1
+++ buildbot/google/docker/buildbot-windows10-vs2019/run.ps1
@@ -4,15 +4,21 @@ $ErrorActionPreference = "Stop"
 # Read password from file
 $WORKER_PASSWORD=Get-Content "C:\volumes\secrets\token"
 
-# Write host information
+# chdir to the volume storing the temporary data
+# This is a mounted volume
+Set-Location c:\volumes\buildbot
+
+# Write host and admin information
 # configure powershell output to use UTF-8, otherwiese buildbot can't read the file
+mkdir "$env:WORKER_NAME\info\"
 $HOST_FILE="$env:WORKER_NAME\info\host"
-$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'; `
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 Write-Output "Windows version: $(cmd /c ver)" > $HOST_FILE
 Write-Output "VCTools version: $env:VCToolsVersion" >> $HOST_FILE
 Write-Output "Cores          : $((Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors)" >> $HOST_FILE
 Write-Output "RAM            : $([int][Math]::Round((Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory / 1073741824)) GB" >> $HOST_FILE
- 
+Write-Output "Christian Kühnel <kuhnel@google.com>" > "$env:WORKER_NAME\info\admin"
+
 # create the worker
 Write-Output "creating worker..."
 buildslave create-slave --keepalive=200 "$env:WORKER_NAME" `
