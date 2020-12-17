4cf14ad11aa3a23f67b0f7561edb7d570bf7aacd
Reid Kleckner <rnk@google.com>
2020-12-17 12:04:44 -0800

    Attempt to fix Python 3 issues in annotated scripts

--- zorg/buildbot/builders/annotated/annotated_builder.py
+++ zorg/buildbot/builders/annotated/annotated_builder.py
@@ -351,7 +351,7 @@ def get_vcvars(vs_tools, arch):
     # Use vswhere.exe if it exists.
     if os.path.exists(VSWHERE_PATH):
         cmd = [VSWHERE_PATH, "-latest", "-property", "installationPath"]
-        vs_path = subprocess.check_output(cmd).strip()
+        vs_path = subprocess.check_output(cmd).decode(sys.stdout.encoding)
         util.report("Running vswhere to find VS: " + repr(cmd))
         util.report("vswhere output: " + vs_path)
         if not os.path.isdir(vs_path):
