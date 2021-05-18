8bbdd35de2d2a8513bbe4f62250ca1cf8f0a38fd
Galina Kistanova <gkistanova@gmail.com>
2021-05-17 20:41:44 -0700

    Added missing comma in clang-ppc64le-rhel builder configuration.

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -633,7 +633,7 @@ all = [
                         "-DCMAKE_C_COMPILER_EXTERNAL_TOOLCHAIN:PATH=/opt/rh/devtoolset-7/root/usr",
                         "-DCMAKE_CXX_COMPILER_EXTERNAL_TOOLCHAIN:PATH=/opt/rh/devtoolset-7/root/usr",
                         "-DLLVM_BINUTILS_INCDIR=/usr/include", "-DBUILD_SHARED_LIBS=ON", "-DLLVM_ENABLE_WERROR=ON",
-                        "-DCMAKE_BUILD_TYPE=Release"
+                        "-DCMAKE_BUILD_TYPE=Release",
                         "-DLLVM_LIT_ARGS=-vj 20"])},
 
     {'name' : "clang-ppc64-aix-ppc64le",
