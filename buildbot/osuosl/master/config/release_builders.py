fbffb6400d33e2f8e7eccf4570eddd3c79b28963
Galina Kistanova <gkistanova@gmail.com>
2021-05-20 16:12:34 -0700

    Limit the number of parallel link jobs for the full LTO rerlease builder.

--- buildbot/osuosl/master/config/release_builders.py
+++ buildbot/osuosl/master/config/release_builders.py
@@ -203,7 +203,11 @@ all = [
     'tags'  : ["clang", "lld", "LTO", "release"],
     'workernames' : ["as-worker-91"],
     'builddir': "clang-with-lto-ubuntu-rel",
-    'factory' : ClangLTOBuilder.getClangWithLTOBuildFactory(jobs=72)},
+    'factory' : ClangLTOBuilder.getClangWithLTOBuildFactory(
+                    jobs=72,
+                    extra_configure_args_lto_stage=[
+                        '-DLLVM_PARALLEL_LINK_JOBS=14',
+                    ])},
 
 
 # Libc++ builders.
