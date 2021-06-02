fccedba38905acda3d156ca84758de85e36538c3
Matt Morehouse <mascasa@google.com>
2021-06-02 12:46:05 -0700

    [sanitizer] Test HWASan + LAM via QEMU.
    Uses LAM-enabled full system QEMU emulation to test HWASan's LAM
    support.
    
    Depends on:
    https://github.com/google/sanitizers/pull/1408
    https://github.com/google/sanitizers/pull/1410
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D103296

--- zorg/buildbot/builders/sanitizers/buildbot_qemu.sh
+++ zorg/buildbot/builders/sanitizers/buildbot_qemu.sh
@@ -23,36 +23,89 @@ build_stage1_clang
 
 COMPILER_BIN_DIR=$(readlink -f ${STAGE1_DIR})/bin
 
-function build_qemu {
-  echo "@@@BUILD_STEP build qemu@@@"
+function git_clone_at_revision {
+  local src_dir_name="${1}"
+  local git_url="${2}"
+  local revision="${3}"
+  local build_dir="${4}"
   (
-    local qemu_url=https://github.com/vitalybuka/qemu.git
-    cd $ROOT
-    [[ -d qemu ]] || git clone $qemu_url || exit 1
-    cd $ROOT/qemu
-    git remote set-url origin $qemu_url
+    cd ${ROOT}
+    [[ -d ${src_dir_name} ]] || git clone ${git_url} ${src_dir_name} || exit 1
+    cd ${ROOT}/${src_dir_name}
+    git remote set-url origin ${git_url}
     git fetch origin
-    [[ "$(git rev-parse HEAD)" == "$(git rev-parse $1)" ]] && $ROOT/qemu_build/qemu-x86_64 --version && exit 0
 
-    rm -rf $ROOT/qemu_build &&
-    mkdir -p $ROOT/qemu_build &&
-    git reset --hard $1 &&
-    git submodule update --init --recursive &&
-    cd $ROOT/qemu_build &&
-    ../qemu/configure --disable-system --enable-linux-user --static &&
+    # Short circuit if we already have a build at the correct revision.
+    [[ "$(git rev-parse HEAD)" == "$(git rev-parse ${revision})" ]] && exit 0
+
+    # Delete the build dir to ensure a rebuild happens at the new revision.
+    rm -rf ${build_dir}
+
+    git reset --hard ${revision}
+    git submodule update --init --recursive
+  )
+}
+
+function build_qemu {
+  local qemu_name="${1}"
+  local qemu_url="${2}"
+  local qemu_revision="${3}"
+
+  local build_dir="${ROOT}/${qemu_name}_build"
+
+  echo "@@@BUILD_STEP build ${qemu_name}@@@"
+  (
+    git_clone_at_revision ${qemu_name} ${qemu_url} ${qemu_revision} \
+      ${build_dir} || exit 1
+
+    ${build_dir}/qemu-x86_64 --version &&
+      ${build_dir}/qemu-system-x86_64 --version &&
+      ${build_dir}/qemu-img --version &&
+      exit 0
+
+    rm -rf ${build_dir} &&
+    mkdir -p ${build_dir} &&
+    cd ${build_dir} &&
+    ${ROOT}/${qemu_name}/configure --enable-linux-user &&
     ninja &&
-    $ROOT/qemu_build/qemu-x86_64 --version
+    ${build_dir}/qemu-x86_64 --version &&
+    ${build_dir}/qemu-system-x86_64 --version &&
+    ${build_dir}/qemu-img --version
   ) || (
     echo "@@@STEP_EXCEPTION@@@"
     exit 2
   )
 }
 
-build_qemu origin/sanitizer_bot
+function build_lam_linux {
+  local build_dir="${ROOT}/lam_linux_build"
 
-BUILDS=
+  echo "@@@BUILD_STEP build lam linux@@@"
+  (
+    git_clone_at_revision lam_linux https://github.com/morehouse/linux.git \
+      origin/lam ${build_dir} || exit 1
 
-function configure_compiler_rt {
+    ls ${build_dir}/arch/x86_64/boot/bzImage && exit 0
+
+    rm -rf ${build_dir} &&
+    mkdir -p ${build_dir} &&
+    cd ${ROOT}/lam_linux &&
+    make O=${build_dir} LD=ld.bfd defconfig &&
+    make O=${build_dir} LD=ld.bfd -j $(nproc) &&
+    ls ${build_dir}/arch/x86_64/boot/bzImage
+  ) || (
+    echo "@@@STEP_EXCEPTION@@@"
+    exit 2
+  )
+}
+
+build_qemu qemu https://github.com/vitalybuka/qemu.git origin/sanitizer_bot
+build_qemu lam_qemu https://github.com/morehouse/qemu.git origin/lam
+build_lam_linux
+
+SCUDO_BUILDS=
+
+function configure_scudo_compiler_rt {
   local arch=$1
   local target="${arch}-linux-gnu${2:-}"
 
@@ -78,7 +131,7 @@ function configure_compiler_rt {
   rm -rf ${out_dir}
   mkdir -p ${out_dir}
 
-  BUILDS+=" ${name}"
+  SCUDO_BUILDS+=" ${name}"
 
   (
     cd ${out_dir}
@@ -115,7 +168,63 @@ function configure_compiler_rt {
   ) &
 }
 
-function run_tests {
+readonly LLVM_SYMBOLIZER_DIR="${ROOT}/llvm_build2_x86_64_symbolizer"
+
+function configure_llvm_symbolizer {
+  rm -rf ${LLVM_SYMBOLIZER_DIR}
+  mkdir -p ${LLVM_SYMBOLIZER_DIR}
+
+  (
+    cd ${LLVM_SYMBOLIZER_DIR}
+
+    (
+      cmake \
+        ${CMAKE_COMMON_OPTIONS} \
+        -DCMAKE_C_COMPILER=${COMPILER_BIN_DIR}/clang \
+        -DCMAKE_CXX_COMPILER=${COMPILER_BIN_DIR}/clang++ \
+        -DLLVM_BUILD_RUNTIME=OFF \
+        -DLLVM_STATIC_LINK_CXX_STDLIB=ON \
+        $LLVM
+     ) >& configure.log
+  ) &
+}
+
+function configure_hwasan_lam {
+  git_clone_at_revision sanitizers https://github.com/google/sanitizers.git \
+    origin/master ""
+  local script="${ROOT}/sanitizers/hwaddress-sanitizer/run_in_qemu_with_lam.sh"
+  ls ${script}
+
+  # Ensure the buildbot start script created a QEMU image for us.
+  ls /b/qemu_image/debian.img
+  ls /b/qemu_image/debian.id_rsa
+
+  # Link to the QEMU image from ROOT, as the QEMU script expects it there.
+  ln -sf /b/qemu_image ${ROOT}/qemu_image
+
+  local out_dir=llvm_build2_x86_64_lam_qemu
+  rm -rf ${out_dir}
+  mkdir -p ${out_dir}
+
+  (
+    cd ${out_dir}
+
+    (
+      cmake \
+        ${CMAKE_COMMON_OPTIONS} \
+        -DLLVM_ENABLE_PROJECTS="clang;compiler-rt;lld;libcxx;libcxxabi" \
+        -DCMAKE_C_COMPILER=${COMPILER_BIN_DIR}/clang \
+        -DCMAKE_CXX_COMPILER=${COMPILER_BIN_DIR}/clang++ \
+        -DLLVM_ENABLE_LLD=ON \
+        -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
+        -DLLVM_LIT_ARGS="-v --time-tests" \
+        -DCOMPILER_RT_EMULATOR="env ROOT=${ROOT} ${script}" \
+        $LLVM
+     ) >& configure.log
+  ) &
+}
+
+function run_scudo_tests {
   local name="${1}"
   local out_dir=llvm_build2_${name}
 
@@ -133,28 +242,62 @@ function run_tests {
   ) || echo "@@@STEP_FAILURE@@@"
 }
 
+function build_llvm_symbolizer {
+  echo "@@@BUILD_STEP llvm-symbolizer x86_64@@@"
+
+  (
+    cd ${LLVM_SYMBOLIZER_DIR}
+
+    cat configure.log
+
+    ninja llvm-symbolizer || exit 3
+  ) || echo "@@@STEP_FAILURE@@@"
+}
+
+function run_hwasan_lam_tests {
+  local name="x86_64_lam_qemu"
+  local out_dir=llvm_build2_${name}
+
+  echo "@@@BUILD_STEP hwasan ${name}@@@"
+
+  (
+    cd ${out_dir}
+
+    cat configure.log
+
+    # LLD must be built first since HWASan tests use -fuse-ld=lld and the
+    # buildbots don't have LLD preinstalled.
+    ninja lld || exit 3
+
+    ninja check-hwasan-lam || exit 3
+  ) || echo "@@@STEP_FAILURE@@@"
+}
+
 echo "@@@BUILD_STEP configure@@@"
 
 for DBG in OFF ON ; do
-  QEMU=0 configure_compiler_rt x86_64
-  configure_compiler_rt x86_64
-  configure_compiler_rt arm eabihf
-  configure_compiler_rt aarch64
-  QEMU_CPU="cortex-a72" configure_compiler_rt aarch64
+  QEMU=0 configure_scudo_compiler_rt x86_64
+  configure_scudo_compiler_rt x86_64
+  configure_scudo_compiler_rt arm eabihf
+  configure_scudo_compiler_rt aarch64
+  QEMU_CPU="cortex-a72" configure_scudo_compiler_rt aarch64
   (
     LINKER_FLAGS="-latomic -Wl,-z,notext -Wno-unused-command-line-argument"
-    configure_compiler_rt mips
-    configure_compiler_rt mipsel
-    configure_compiler_rt mips64 abi64
-    configure_compiler_rt mips64el abi64
+    configure_scudo_compiler_rt mips
+    configure_scudo_compiler_rt mipsel
+    configure_scudo_compiler_rt mips64 abi64
+    configure_scudo_compiler_rt mips64el abi64
   )
-  
-  configure_compiler_rt powerpc64le
+
+  configure_scudo_compiler_rt powerpc64le
 done
+configure_llvm_symbolizer
+configure_hwasan_lam
 
 wait
 
-for B in $BUILDS ; do
-  run_tests $B
-done 
-
+for B in $SCUDO_BUILDS ; do
+  run_scudo_tests $B
+done
+build_llvm_symbolizer
+run_hwasan_lam_tests
