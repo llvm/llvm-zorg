99821277c29d5565a8f5c6cda80ea8b0faf097b4
Simon Moll <simon.moll@emea.nec.com>
2021-06-08 11:14:37 +0200

    [VE] disable cmake caches until resolved

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -1701,7 +1701,6 @@ all = [
                     clean=True,
                     depends_on_projects=['llvm','clang','openmp'],
                     extra_configure_args=[
-                        "-C=~/llvm_ve_caches/llvm_ve_runtimes.cmake",
                         "-DLLVM_TARGETS_TO_BUILD=X86",
                         "-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=VE",
                         "-DLLVM_ENABLE_RUNTIMES=compiler-rt",
