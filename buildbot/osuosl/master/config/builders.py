4a20a6f60b4291bd962b55e99a558f8a6f759619
Ron Lieberman <ron.lieberman@amd.com>
2021-09-09 13:11:03 -0500

    Revert "[openmp][amdgpu] Remove dependencies clause [llvm,clang,lld] from project"
    This reverts commit f2ab06acd09aa700cdfa461545a1d40056c0c092.

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -1398,6 +1398,7 @@ all = [
     'factory' : OpenMPBuilder.getOpenMPCMakeBuildFactory(
                         clean=False,
                         enable_runtimes=[],
+                        depends_on_projects=['llvm','clang','lld'],
                         extraCmakeArgs=[
                             "-DCMAKE_BUILD_TYPE=Release",
                             "-DCLANG_DEFAULT_LINKER=lld",
