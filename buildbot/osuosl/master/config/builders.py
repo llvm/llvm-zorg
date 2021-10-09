856646ccf14eb08adc29fef8d4a591465b835791
Ron Lieberman <Ron.Lieberman@amd.com>
2021-10-09 06:28:02 -0400

    [builder][amdgpu][openmp] disable use of ccache for now

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -1508,8 +1508,6 @@ all += [
                             "-DLLVM_TARGETS_TO_BUILD=X86;AMDGPU",
                             "-DLLVM_ENABLE_ASSERTIONS=ON",
                             "-DLLVM_ENABLE_RUNTIMES=openmp",
-                            "-DCMAKE_C_COMPILER_LAUNCHER=ccache",
-                            "-DCMAKE_CXX_COMPILER_LAUNCHER=ccache",
                             ],
                         install=True,
                         testsuite=False,
