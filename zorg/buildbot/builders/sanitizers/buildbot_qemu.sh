e5928c07d1048f899ed6e46c56b29d9d97929e85
Vitaly Buka <vitalybuka@google.com>
2022-07-08 20:52:02 -0700

    [sanitizer] Attempt to fix qemu script

--- zorg/buildbot/builders/sanitizers/buildbot_qemu.sh
+++ zorg/buildbot/builders/sanitizers/buildbot_qemu.sh
@@ -65,7 +65,6 @@ function setup_lam_qemu_image {
     set +x
     echo "$MISSING_QEMU_IMAGE_MESSAGE"
     build_exception
-    exit 1
   }
 }
 
@@ -130,7 +129,7 @@ function build_qemu {
 function build_lam_linux {
   echo "@@@BUILD_STEP build lam linux@@@"
   local build_dir="${ROOT}/lam_linux_build"
-  LAM_KERNEL=${BUILD_STEP}/arch/x86_64/boot/bzImage
+  LAM_KERNEL="${BUILD_STEP}/arch/x86_64/boot/bzImage"
   (
     git_clone_at_revision lam_linux https://github.com/morehouse/linux.git \
       origin/lam ${build_dir} || exit 1
