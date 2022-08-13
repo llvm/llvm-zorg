f2b33a92bba69ff2419be00e8347c2fed6765a22
Vitaly Buka <vitalybuka@google.com>
2022-08-12 22:56:57 -0700

    [sanitizer] Another attempt to control LIT options

--- zorg/buildbot/builders/sanitizers/buildbot_functions.sh
+++ zorg/buildbot/builders/sanitizers/buildbot_functions.sh
@@ -20,7 +20,8 @@ echo @@@BUILD_STEP Prepare@@@
 BUILDBOT_CLOBBER="${BUILDBOT_CLOBBER:-}"
 BUILDBOT_REVISION="${BUILDBOT_REVISION:-origin/main}"
 
-CMAKE_COMMON_OPTIONS="-DLLVM_LIT_ARGS=--time-tests"
+export LIT_OPTS=-time-tests
+CMAKE_COMMON_OPTIONS="-DLLVM_LIT_ARGS=-v"
 
 function rm_dirs {
   while ! rm -rf $@ ; do sleep 1; done
