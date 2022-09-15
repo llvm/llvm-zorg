2aceadcebd0b0c01cd84f8c1dacc2684c68aa9f1
Vitaly Buka <vitalybuka@google.com>
2022-09-15 13:58:02 -0700

    [sanitizer] Increase timeouts

--- zorg/buildbot/builders/sanitizers/buildbot_functions.sh
+++ zorg/buildbot/builders/sanitizers/buildbot_functions.sh
@@ -315,17 +315,19 @@ function check_stage2 {
 
   echo @@@BUILD_STEP stage2/$sanitizer_name check-cxxabi@@@
   (
+    # Very slow.
+    export LIT_FILTER_OUT="modules_include.sh.cpp"
     if [[ "$(arch)" == "aarch64" && "$sanitizer_name" == "asan" ]] ; then
       # TODO: Investigate one leak and two slowest tests.
-      export LIT_FILTER_OUT="test_vector2.pass.cpp|catch_multi_level_pointer.pass.cpp|guard_threaded_test.pass.cpp"
+      LIT_FILTER_OUT+="|test_vector2.pass.cpp|catch_multi_level_pointer.pass.cpp|guard_threaded_test.pass.cpp"
     fi
     if [[ "$(arch)" == "aarch64" && "$sanitizer_name" == "msan" ]] ; then
       # TODO: Investigate one slow tests.
-      export LIT_FILTER_OUT="catch_multi_level_pointer.pass.cpp|guard_threaded_test.pass.cpp|test_demangle.pass.cpp"
+      LIT_FILTER_OUT+="|catch_multi_level_pointer.pass.cpp|guard_threaded_test.pass.cpp|test_demangle.pass.cpp"
     fi
     if [[ "$(arch)" == "aarch64" && "$sanitizer_name" == "hwasan" ]] ; then
       # TODO: Investigate one slow tests.
-      export LIT_FILTER_OUT="catch_multi_level_pointer.pass.cpp|guard_threaded_test.pass.cpp|test_demangle.pass.cpp"
+      LIT_FILTER_OUT+="|catch_multi_level_pointer.pass.cpp|guard_threaded_test.pass.cpp|test_demangle.pass.cpp"
     fi
     ninja -C libcxx_build_${sanitizer_name} check-cxxabi
   ) || build_failure
