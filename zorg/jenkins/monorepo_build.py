7d700dae8fc2c898b5ed4804eacff0a8d598e819
Med Ismail Bennani <medismail.bennani@gmail.com>
2023-02-16 13:55:43 -0800

    [jenkins] Update monorepo_build.py to use system compiler
    This patch updates the monorepo_build script to use the system compiler
    instead of the host built compiler.
    
    This should address a build failure in lldb incremental bot, that is
    happening when assertion are enabled.
    
    Signed-off-by: Med Ismail Bennani <medismail.bennani@gmail.com>

--- zorg/jenkins/monorepo_build.py
+++ zorg/jenkins/monorepo_build.py
@@ -176,12 +176,11 @@ class Configuration(object):
 
     def CC(self):
         """Location of the host compiler, if one is present in this build."""
-        cc_basedir = os.path.join(self.workspace, 'host-compiler/')
-        if os.path.exists(cc_basedir):
-            clang_exec_path = os.path.join(cc_basedir, 'bin/clang')
-            assert os.path.exists(clang_exec_path), "host-compiler present," \
-                                                    " but has no clang executable."
-            return clang_exec_path
+        system_clang_path = query_sys_tool("macosx", "clang")
+        if system_clang_path:
+            assert os.path.exists(system_clang_path), "host-compiler present," \
+                                                      " but has no clang executable."
+            return system_clang_path
         else:
             return False
 
