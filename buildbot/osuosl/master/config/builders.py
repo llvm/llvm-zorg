f0771b61d210ff4ab714b7dce7d5f6bcfbc38f45
JP Lehr <JanPatrick.Lehr@amd.com>
2023-03-30 17:11:40 -0400

    Revert "Reland [OpenMP][LibC] Adds testing LibC GPU Mode for AMDGPU"
    This reverts commit 29a9b33361589f9e6ac23c683c3c6a271d1483d3.

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -1874,19 +1874,17 @@ all += [
     'builddir': "openmp-offload-amdgpu-runtime-experimental",
     'factory' : OpenMPBuilder.getOpenMPCMakeBuildFactory(
                         clean=True,
-                        depends_on_projects=['llvm', 'clang', 'compiler-rt', 'libc', 'lld', 'openmp'],
+                        enable_runtimes=['openmp'],
+                        depends_on_projects=['llvm','clang','lld','openmp'],
                         extraCmakeArgs=[
                             "-DCMAKE_BUILD_TYPE=Release",
                             "-DCLANG_DEFAULT_LINKER=lld",
                             "-DLLVM_TARGETS_TO_BUILD=X86;AMDGPU",
                             "-DLLVM_ENABLE_ASSERTIONS=ON",
+                            "-DLLVM_ENABLE_RUNTIMES=openmp",
                             "-DCMAKE_C_COMPILER_LAUNCHER=ccache",
                             "-DCMAKE_CXX_COMPILER_LAUNCHER=ccache",
                             "-DLIBOMPTARGET_FOUND_AMDGPU_GPU=ON",
-                            "-DLLVM_LIBC_FULL_BUILD=ON",
-                            "-DLIBC_GPU_BUILD=ON",
-                            "-DLIBC_GPU_ARCHITECTURE=gfx906",
-                            "-DLIBC_GPU_TEST_ARCHITECTURE=gfx906",
                             ],
                         install=True,
                         testsuite=False,
@@ -1895,7 +1893,7 @@ all += [
                             "-DTEST_SUITE_SOLLVEVV_OFFLOADING_CFLAGS=-fopenmp;-fopenmp-targets=amdgcn-amd-amdhsa;-Xopenmp-target=amdgcn-amd-amdhsa;-march=gfx906",
                             "-DTEST_SUITE_SOLLVEVV_OFFLOADING_LDLAGS=-fopenmp;-fopenmp-targets=amdgcn-amd-amdhsa;-Xopenmp-target=amdgcn-amd-amdhsa;-march=gfx906",
                         ],
-                        add_lit_checks=["check-clang", "check-llvm", "check-lld", "check-libc"]
+                        add_lit_checks=["check-clang", "check-llvm", "check-lld"]
                     )},
 
 
