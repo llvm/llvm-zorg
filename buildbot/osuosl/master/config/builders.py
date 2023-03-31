ef894257d1001f4b13c87737c64d5f22b92e7782
JP Lehr <JanPatrick.Lehr@amd.com>
2023-03-30 21:34:55 -0500

    [OpenMP][LibC] Adds testing LibC GPU Mode for AMDGPU
    Summary:
    This adds testing LibC's GPU mode to the AMDGPU (experimental) buildbot.
    The change requires the buildbot to also depend on the libc project and
    pass-in a few more CMake flags.
    Finally, it runs the additional check-libc target.
    
    Differential Revision: https://reviews.llvm.org/D146722

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -1874,17 +1874,18 @@ all += [
     'builddir': "openmp-offload-amdgpu-runtime-experimental",
     'factory' : OpenMPBuilder.getOpenMPCMakeBuildFactory(
                         clean=True,
-                        enable_runtimes=['openmp'],
-                        depends_on_projects=['llvm','clang','lld','openmp'],
+                        depends_on_projects=['llvm', 'clang', 'compiler-rt', 'libc', 'lld', 'openmp'],
                         extraCmakeArgs=[
                             "-DCMAKE_BUILD_TYPE=Release",
                             "-DCLANG_DEFAULT_LINKER=lld",
                             "-DLLVM_TARGETS_TO_BUILD=X86;AMDGPU",
                             "-DLLVM_ENABLE_ASSERTIONS=ON",
-                            "-DLLVM_ENABLE_RUNTIMES=openmp",
                             "-DCMAKE_C_COMPILER_LAUNCHER=ccache",
                             "-DCMAKE_CXX_COMPILER_LAUNCHER=ccache",
                             "-DLIBOMPTARGET_FOUND_AMDGPU_GPU=ON",
+                            "-DLIBC_GPU_BUILD=ON",
+                            "-DLIBC_GPU_ARCHITECTURES=gfx906",
+                            "-DLIBC_GPU_TEST_ARCHITECTURE=gfx906",
                             ],
                         install=True,
                         testsuite=False,
@@ -1893,7 +1894,7 @@ all += [
                             "-DTEST_SUITE_SOLLVEVV_OFFLOADING_CFLAGS=-fopenmp;-fopenmp-targets=amdgcn-amd-amdhsa;-Xopenmp-target=amdgcn-amd-amdhsa;-march=gfx906",
                             "-DTEST_SUITE_SOLLVEVV_OFFLOADING_LDLAGS=-fopenmp;-fopenmp-targets=amdgcn-amd-amdhsa;-Xopenmp-target=amdgcn-amd-amdhsa;-march=gfx906",
                         ],
-                        add_lit_checks=["check-clang", "check-llvm", "check-lld"]
+                        add_lit_checks=["check-clang", "check-llvm", "check-lld", "check-libc"]
                     )},
 
 
