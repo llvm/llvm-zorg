7b76fb1ee2fb73c6d121b39c86cd2c76677385e5
Vitaly Buka <vitalybuka@google.com>
2023-08-16 16:19:50 -0700

    [sanitizer] Switch to check-compiler-rt and remove some dead code

--- zorg/buildbot/builders/sanitizers/buildbot_cmake.sh
+++ zorg/buildbot/builders/sanitizers/buildbot_cmake.sh
@@ -26,7 +26,6 @@ SUPPORTS_32_BITS=${SUPPORTS_32_BITS:-1}
 ZLIB=$ROOT/zlib
 
 CMAKE_COMMON_OPTIONS+=" -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_PARALLEL_LINK_JOBS=10 -DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=OFF ${CMAKE_ARGS}"
-ENABLE_LIBCXX_FLAG=
 
 if [ -e /usr/include/plugin-api.h ]; then
   CMAKE_COMMON_OPTIONS+=" -DLLVM_BINUTILS_INCDIR=/usr/include"
@@ -34,53 +33,10 @@ fi
 
 CMAKE_COMMON_OPTIONS+=" -DLLVM_ENABLE_PROJECTS='clang;compiler-rt'"
 
-CHECK_LIBCXX=${CHECK_LIBCXX:-1}
-CHECK_SYMBOLIZER=${CHECK_SYMBOLIZER:-$CHECK_LIBCXX}
 # FIXME: Something broken with LLD switch 19cb7a33e82.
 CHECK_SYMBOLIZER=0
-CHECK_LLD=${CHECK_LLD:-1}
-CHECK_ASAN=0
-CHECK_HWASAN=0
-CHECK_UBSAN=0
-CHECK_MSAN=0
-CHECK_TSAN=0
-CHECK_LSAN=0
-CHECK_DFSAN=0
-CHECK_SCUDO_STANDALONE=0
-CHECK_CFI=0
 case "$ARCH" in
-  x86_64)
-    CHECK_ASAN=1
-    CHECK_HWASAN=1
-    CHECK_UBSAN=1
-    CHECK_MSAN=1
-    CHECK_TSAN=1
-    CHECK_LSAN=1
-    CHECK_DFSAN=1
-    CHECK_SCUDO_STANDALONE=1
-    CHECK_CFI=1
-  ;;
-  aarch64)
-    CHECK_ASAN=1
-    CHECK_UBSAN=1
-    CHECK_MSAN=1
-    CHECK_TSAN=1
-    CHECK_LSAN=1
-    CHECK_DFSAN=1
-  ;;
-  mips64*)
-    CHECK_ASAN=1
-    CHECK_UBSAN=1
-    CHECK_MSAN=1
-    CHECK_TSAN=1
-    CHECK_LSAN=1
-    CHECK_DFSAN=1
-  ;;
   ppc64*)
-    CHECK_ASAN=1
-    CHECK_UBSAN=1
-    CHECK_MSAN=1
-    CHECK_TSAN=1
     CMAKE_COMMON_OPTIONS+=" -DLLVM_TARGETS_TO_BUILD=PowerPC"
     if [[ "$ARCH" == "ppc64le" ]]; then
       CMAKE_COMMON_OPTIONS+=" -DLLVM_LIT_ARGS=-vj256"
@@ -88,34 +44,11 @@ case "$ARCH" in
       CMAKE_COMMON_OPTIONS+=" -DLLVM_LIT_ARGS=-vj80"
     fi
   ;;
-  i*86)
-    CHECK_UBSAN=1
-    CHECK_ASAN=1
-  ;;
-  mips*)
-    CHECK_UBSAN=1
-    CHECK_ASAN=1
-  ;;
-  arm*)
-    CHECK_UBSAN=1
-    CHECK_ASAN=1
-  ;;
-  s390x)
-    CHECK_UBSAN=1
-  ;;
 esac
 
-PROJECTS="clang;compiler-rt"
-if [[ "$CHECK_LLD" != "0" ]]; then
-  PROJECTS+=";lld"
-fi
+PROJECTS="clang;compiler-rt;lld"
 CMAKE_COMMON_OPTIONS+=" -DLLVM_ENABLE_PROJECTS='${PROJECTS}' -DLLVM_ENABLE_RUNTIMES=libcxx;libcxxabi"
-
-if [[ "$CHECK_CFI" == "1" ]]; then
-  # We need this after https://reviews.llvm.org/D62050
-  CMAKE_COMMON_OPTIONS+=" -DLLVM_BUILD_LLVM_DYLIB=ON"
-fi
-
+CMAKE_COMMON_OPTIONS+=" -DLLVM_BUILD_LLVM_DYLIB=ON"
 CMAKE_COMMON_OPTIONS+=" -DLLVM_LIT_ARGS=-v"
 
 buildbot_update
@@ -129,39 +62,12 @@ echo @@@BUILD_STEP build fresh toolchain@@@
 mkdir -p clang_build
 (cd clang_build && cmake ${CMAKE_COMMON_OPTIONS} $LLVM ) || (rm -rf clang_build ; build_failure)
 
-BOOTSTRAP_BUILD_TARGETS="clang"
-if [[ "$CHECK_LLD" != "0" ]]; then
-  BOOTSTRAP_BUILD_TARGETS="$BOOTSTRAP_BUILD_TARGETS lld"
-fi
-(cd clang_build && ninja $BOOTSTRAP_BUILD_TARGETS) || build_failure
-
-# If we're building with libcxx, install the headers to clang_build/include.
-if [ ! -z ${ENABLE_LIBCXX_FLAG} ]; then
-(cd clang_build && ninja -C ${LIBCXX} installheaders \
-  HEADER_DIR=${PWD}/include) || build_failure
-fi
+(cd clang_build && ninja clang lld) || build_failure
 
 # Check on Linux: build and test sanitizers using gcc as a host
 # compiler.
-check_in_gcc() {
-  CONDITION=$1
-  SANITIZER=$2
-  if [ "$CONDITION" == "1" ]; then
-    echo @@@BUILD_STEP check-$SANITIZER in gcc build@@@
-    (cd clang_build && ninja check-$SANITIZER) || build_failure
-  fi
-}
-check_in_gcc 1 sanitizer
-check_in_gcc $CHECK_ASAN asan
-check_in_gcc $CHECK_HWASAN hwasan
-check_in_gcc $CHECK_CFI cfi-and-supported
-check_in_gcc $CHECK_DFSAN dfsan
-check_in_gcc $CHECK_LSAN lsan
-check_in_gcc $CHECK_MSAN msan
-check_in_gcc $CHECK_SCUDO_STANDALONE scudo_standalone
-LDFLAGS=-no-pie check_in_gcc $CHECK_TSAN tsan
-check_in_gcc $CHECK_UBSAN ubsan
-check_in_gcc $CHECK_UBSAN ubsan-minimal
+echo @@@BUILD_STEP check-compiler-rt in gcc build@@@
+(cd clang_build && ninja check-compiler-rt) || build_failure
 
 ### From now on we use just-built Clang as a host compiler ###
 CLANG_PATH=${ROOT}/clang_build/bin
@@ -172,50 +78,11 @@ echo @@@BUILD_STEP bootstrap clang@@@
 mkdir -p llvm_build64
 (cd llvm_build64 && cmake ${CMAKE_CLANG_OPTIONS} \
                           -DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON \
-                          ${ENABLE_LIBCXX_FLAG} \
                           $LLVM) || build_failure
-
-# First, build only Clang.
-(cd llvm_build64 && ninja clang) || build_failure
-
-# If needed, install the headers to clang_build/include.
-if [ ! -z ${ENABLE_LIBCXX_FLAG} ]; then
-  (cd llvm_build64 && ninja -C ${LIBCXX} installheaders \
-    HEADER_DIR=${PWD}/include) || build_failure
-fi
 
 # Now build everything else.
 (cd llvm_build64 && ninja) || build_failure
-# Symbolizer dependencies.
-(cd llvm_build64 && ninja llvm-ar llvm-link llvm-tblgen opt) || build_failure
 
-
-if [ "$CHECK_CFI" == "1" ]; then
-  # FIXME: Make these true dependencies of check-cfi-and-supported when
-  # compiler-rt is configured as an external project.
-  (cd llvm_build64 && ninja LLVMgold opt sanstats) || build_failure
-fi
-
-check_64bit() {
-  CONDITION=$1
-  SANITIZER=$2
-  if [ "$CONDITION" == "1" ]; then
-    echo @@@BUILD_STEP 64-bit check-$SANITIZER@@@
-    (cd llvm_build64 && ninja check-$SANITIZER) || build_failure
-  fi
-}
-
-check_64bit 1 sanitizer
-check_64bit $CHECK_ASAN asan
-check_64bit $CHECK_ASAN asan-dynamic
-check_64bit $CHECK_HWASAN hwasan
-check_64bit $CHECK_CFI cfi-and-supported
-check_64bit $CHECK_DFSAN dfsan
-check_64bit $CHECK_LSAN lsan
-check_64bit $CHECK_MSAN msan
-check_64bit $CHECK_TSAN tsan
-check_64bit $CHECK_UBSAN ubsan
-check_64bit $CHECK_UBSAN ubsan-minimal
 
 echo @@@BUILD_STEP 64-bit check-compiler-rt@@@
 (cd llvm_build64 && ninja check-compiler-rt) || build_failure
@@ -248,29 +115,8 @@ CMAKE_NINJA_OPTIONS="${CMAKE_CLANG_OPTIONS} -GNinja"
 ln -sf llvm_build_ninja/compile_commands.json $LLVM
 (cd llvm_build_ninja && ninja) || build_failure
 
-check_ninja() {
-  CONDITION=$1
-  SANITIZER=$2
-  if [ "$CONDITION" == "1" ]; then
-    echo @@@BUILD_STEP ninja check-$SANITIZER@@@
-    (cd llvm_build_ninja && ninja check-$SANITIZER) || build_failure
-  fi
-}
-
-check_ninja 1 sanitizer
-check_ninja $CHECK_ASAN asan
-check_ninja $CHECK_HWASAN hwasan
-check_ninja $CHECK_CFI cfi-and-supported
-check_ninja $CHECK_DFSAN dfsan
-check_ninja $CHECK_LSAN lsan
-check_ninja $CHECK_MSAN msan
-check_ninja $CHECK_SCUDO_STANDALONE scudo_standalone
-check_ninja $CHECK_TSAN tsan
-check_ninja $CHECK_UBSAN ubsan
-check_ninja $CHECK_UBSAN ubsan-minimal
-
 echo @@@BUILD_STEP ninja check-compiler-rt@@@
-(cd llvm_build_ninja && ninja check-compiler-rt) || true
+(cd llvm_build_ninja && ninja check-compiler-rt) || build_failure
 
 if [ "$CHECK_SYMBOLIZER" == "1" ]; then
   build_symbolizer() {
@@ -291,26 +137,8 @@ if [ "$CHECK_SYMBOLIZER" == "1" ]; then
   build_symbolizer 32 llvm_build_ninja
   build_symbolizer 64 llvm_build_ninja
 
-  check_ninja_with_symbolizer() {
-    CONDITION=$1
-    SANITIZER=$2
-    if [ "$CONDITION" == "1" ]; then
-      echo @@@BUILD_STEP ninja check-$SANITIZER with internal symbolizer@@@
-      (cd llvm_build_ninja && ninja check-$SANITIZER) || build_failure
-    fi
-  }
-
-  # TODO: Replace LIT_FILTER_OUT with lit features.
-  LIT_FILTER_OUT=":: (max_allocation_size.cpp|Linux/soft_rss_limit_mb_test.cpp)$" check_ninja_with_symbolizer 1 sanitizer
-  LIT_FILTER_OUT=":: TestCases/Linux/check_memcpy.c$" check_ninja_with_symbolizer $CHECK_ASAN asan
-  # check_ninja_with_symbolizer $CHECK_HWASAN hwasan
-  # check_ninja_with_symbolizer $CHECK_CFI cfi-and-supported
-  check_ninja_with_symbolizer $CHECK_DFSAN dfsan
-  LIT_FILTER_OUT=":: TestCases/(realloc_too_big.c|recoverable_leak_check.cpp|suppressions_file.cpp)$" check_ninja_with_symbolizer $CHECK_LSAN lsan
-  LIT_FILTER_OUT=":: Linux/check_memcpy.c$" check_ninja_with_symbolizer $CHECK_MSAN msan
-  check_ninja_with_symbolizer $CHECK_SCUDO_STANDALONE scudo_standalone
-  LIT_FILTER_OUT=":: Linux/check_memcpy.c$" check_ninja_with_symbolizer $CHECK_TSAN tsan
-  LIT_FILTER_OUT=":: TestCases/TypeCheck/(vptr-virtual-base.cpp|vptr.cpp)$" check_ninja_with_symbolizer $CHECK_UBSAN ubsan
+  echo @@@BUILD_STEP ninja check-compiler-rt with internal symbolizer@@@
+  (cd llvm_build_ninja && ninja check-compiler-rt) || build_failure
 fi
 
 cleanup
