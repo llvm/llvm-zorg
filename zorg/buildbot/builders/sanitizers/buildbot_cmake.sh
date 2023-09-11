be79c5e5a03295d11a7dbc12573a7631630bbe96
Vitaly Buka <vitalybuka@google.com>
2023-09-10 23:08:09 -0700

    [sanitizer] Copy all steps of buildbot_standard.sh

--- zorg/buildbot/builders/sanitizers/buildbot_cmake.sh
+++ zorg/buildbot/builders/sanitizers/buildbot_cmake.sh
@@ -51,15 +51,20 @@ CMAKE_COMMON_OPTIONS+=" ${CMAKE_ARGS}"
 
 buildbot_update
 
-function build_and_test {
+function build {
   local build_dir=llvm_build64
-  echo "@@@BUILD_STEP build compiler-rt ${1}@@@"
+  echo "@@@BUILD_STEP build compiler-rt ${1}${2}@@@"
   rm -rf ${build_dir}
   mkdir -p ${build_dir}
 
   cmake -B ${build_dir} ${CMAKE_COMMON_OPTIONS} ${2} $LLVM || build_failure
   ninja -C ${build_dir} || build_failure
+}
 
+function build_and_test {
+  build "${1}" "${2}"
+
+  local build_dir=llvm_build64
   echo "@@@BUILD_STEP test compiler-rt ${1}@@@"
   ninja -C ${build_dir} check-compiler-rt || build_failure
 }
@@ -70,8 +75,15 @@ CMAKE_COMMON_OPTIONS+=" ${STAGE1_AS_COMPILER}"
 CMAKE_COMMON_OPTIONS+=" -DLLVM_ENABLE_WERROR=ON"
 
 if [ "$CHECK_SYMBOLIZER" == "1" ]; then
-  build_and_test "with internal symbolizer" "-DCOMPILER_RT_ENABLE_INTERNAL_SYMBOLIZER=ON"
+  build_and_test "" "-DCOMPILER_RT_ENABLE_INTERNAL_SYMBOLIZER=ON"
 fi
+
+# FIXME: all tests should pass with DCOMPILER_RT_DEBUG=ON
+LIT_FILTER_OUT='(AddressSanitizer|asan|ubsan)' \
+  build_and_test "" "-DCOMPILER_RT_DEBUG=ON"
+
+# Copied from buildbot_standard.sh, where it was not tested as well.
+build "" "-DCOMPILER_RT_DEBUG=ON -DCOMPILER_RT_TSAN_DEBUG_OUTPUT=ON -DLLVM_INCLUDE_TESTS=OFF"
 
 build_and_test "" ""
 
@@ -90,5 +102,11 @@ ninja -C compiler_rt_build || build_failure
 
 echo @@@BUILD_STEP test standalone compiler-rt@@@
 ninja -C compiler_rt_build check-all || build_failure
+
+# FIXME: Convert to a LIT test.
+echo @@@BUILD_STEP tsan analyze@@@
+BIN=tsan_bin
+echo "int main() {return 0;}" | ${FRESH_CLANG_PATH}/clang -x c++ - -fsanitize=thread -O2 -o ${BIN}
+$LLVM/../compiler-rt/lib/tsan/check_analyze.sh ${BIN} || build_failure
 
 cleanup
