d3811c73450c1195cb02b6585dc282b2e012e806
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2023-11-03 13:38:21 +0100

    Add OpenMP builder for s390x

--- buildbot/osuosl/master/config/status.py
+++ buildbot/osuosl/master/config/status.py
@@ -91,7 +91,8 @@ def getReporters():
             subject = "Build %(builder)s Failure",
             mode = "failing",
             builders = ["clang-s390x-linux", "clang-s390x-linux-multistage",
-                        "clang-s390x-linux-lnt", "mlir-s390x-linux"]),
+                        "clang-s390x-linux-lnt", "mlir-s390x-linux",
+                        "openmp-s390x-linux"]),
         reporters.MailNotifier(
             fromaddr = "llvm.buildmaster@lab.llvm.org",
             sendToInterestedUsers = False,
