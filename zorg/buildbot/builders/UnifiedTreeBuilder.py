5492e9657e2cd033a6e803e5ef57517caae2d5b1
Mehdi Amini <joker.eph@gmail.com>
2024-03-13 00:34:45 -0700

    Revert "Try adding an option to automatically install MLIR pip requirements t…"
    This reverts commit 74bfd396e6a04f3927a54c73c12478e1dd855f17.

--- zorg/buildbot/builders/UnifiedTreeBuilder.py
+++ zorg/buildbot/builders/UnifiedTreeBuilder.py
@@ -251,7 +251,6 @@ def getCmakeBuildFactory(
            install_dir = None,
            clean = False,
            extra_configure_args = None,
-           install_pip_requirements = False,
            env = None,
            **kwargs):
 
@@ -265,13 +264,6 @@ def getCmakeBuildFactory(
             **kwargs) # Pass through all the extra arguments.
 
     cleanBuildRequested = lambda step: step.build.getProperty("clean", default=step.build.getProperty("clean_obj")) or clean
-
-    if install_pip_requirements:
-        # Install python requirements, right now for MLIR
-        # but can evolve to more projects later.
-        f.addStep(steps.ShellCommand(command=["pip", "install", "-q", "-r", "mlir/python/requirements.txt"],
-                                     workdir=llvm_srcdir]))
-
     addCmakeSteps(
         f,
         cleanBuildRequested=cleanBuildRequested,
@@ -295,7 +287,6 @@ def getCmakeWithNinjaBuildFactory(
            install_dir = None,
            clean = False,
            extra_configure_args = None,
-           install_pip_requirements = False,
            env = None,
            **kwargs):
 
@@ -330,7 +321,6 @@ def getCmakeWithNinjaBuildFactory(
             install_dir=install_dir,
             clean=clean,
             extra_configure_args=cmake_args,
-            install_pip_requirements=install_pip_requirements,
             env=merged_env,
             **kwargs) # Pass through all the extra arguments.
 
