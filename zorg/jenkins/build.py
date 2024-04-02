006804171fbf120928d3875e2026fd83cb386b4f
Justice Adams <107649528+justice-adams-apple@users.noreply.github.com>
2024-04-02 09:27:43 -0700

    Workaround Stage 2 bots being broken

--- zorg/jenkins/build.py
+++ zorg/jenkins/build.py
@@ -428,6 +428,7 @@ def clang_builder(target):
                                    '-DCLANG_INCLUDE_TESTS=On',
                                    '-DLLVM_INCLUDE_UTILS=On',
                                    '-DCMAKE_MACOSX_RPATH=On',
+                                   '-DLLVM_ENABLE_MODULES=Off', # Workaround Stage 2 Green Dragon being broken
                                    ]
 
             if conf.llvm_enable_runtimes:
