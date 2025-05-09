600a714fec271c1351bdff18df5b9fb0e6998a07
Aiden Grossman <aidengrossman@google.com>
2025-05-09 07:15:37 +0000

    [CI] Rename premerge_resources modules
    This patch renames the premerge_resources modules to be more consistent with
    the rest of the naming using the name of the cluster with a suffix of the
    cluster region.

--- premerge/main.tf
+++ premerge/main.tf
@@ -128,7 +128,7 @@ provider "kubernetes" {
   alias                  = "llvm-premerge-us-west"
 }
 
-module "premerge_cluster_resources" {
+module "premerge_cluster_resources_us_central" {
   source                     = "./premerge_resources"
   github_app_id              = data.google_secret_manager_secret_version.github_app_id.secret_data
   github_app_installation_id = data.google_secret_manager_secret_version.github_app_installation_id.secret_data
@@ -141,7 +141,13 @@ module "premerge_cluster_resources" {
   }
 }
 
-module "premerge_cluster_resources2" {
+# TODO(boomanaiden154): Remove this after moving.
+moved {
+  from = module.premerge_cluster_resources
+  to   = module.premerge_cluster_resources_us_central
+}
+
+module "premerge_cluster_resources_us_west" {
   source                     = "./premerge_resources"
   github_app_id              = data.google_secret_manager_secret_version.github_app_id.secret_data
   github_app_installation_id = data.google_secret_manager_secret_version.github_app_installation_id.secret_data
@@ -152,6 +158,12 @@ module "premerge_cluster_resources2" {
     kubernetes = kubernetes.llvm-premerge-us-west
     helm       = helm.llvm-premerge-us-west
   }
+}
+
+# TODO(boomanaiden154): Remove this after moving.
+moved {
+  from = module.premerge_cluster_resources2
+  to   = module.premerge_cluster_resources_us_west
 }
 
 data "google_secret_manager_secret_version" "metrics_github_pat" {
