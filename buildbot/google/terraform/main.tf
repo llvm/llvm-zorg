8260d6a3407712a86694b9d28193e4b8424dee19
Aiden Grossman <aidengrossman@google.com>
2025-07-17 09:30:44 -0700

    Format all terraform files
    Run terraform fmt -recursive . across the whole repository. A couple
    buildbot/google files get formatted during this process.
    
    Reviewers: lnihlen, Keenuts, cmtice
    
    Reviewed By: cmtice
    
    Pull Request: https://github.com/llvm/llvm-zorg/pull/502

--- buildbot/google/terraform/main.tf
+++ buildbot/google/terraform/main.tf
@@ -5,15 +5,15 @@
 # ~/.config/gcloud/legacy_credentials/<your email>/adc.json
 terraform {
   backend "gcs" {
-    bucket  = "buildbot_cluster_terraform_backend"
-    prefix  = "terraform/state"
+    bucket = "buildbot_cluster_terraform_backend"
+    prefix = "terraform/state"
   }
 }
 
 # configure Google Cloud project
 provider "google" {
-  project  = var.gcp_config.project
-  region   = var.gcp_config.region
+  project = var.gcp_config.project
+  region  = var.gcp_config.region
 }
 
 
@@ -25,10 +25,10 @@ resource "google_compute_network" "vpc_network" {
 
 # Create the cluster runningn all Kubernetes services
 resource "google_container_cluster" "primary" {
-  name     = "buildbot-cluster"
+  name = "buildbot-cluster"
   # maybe have a regional cluster for Kubernetes, as we depend on this...
   location = var.gcp_config.zone_a
-  
+
   # configure local network, required for Kubernetes on Windows 
   network = google_compute_network.vpc_network.name
   # enable alias IP addresses, required for Kubernetes for Windows
@@ -51,24 +51,24 @@ resource "google_container_cluster" "primary" {
 }
 
 resource "google_container_node_pool" "linux_16_core_pool" {
-  name       = "linux-16-core-pool"
+  name = "linux-16-core-pool"
   # specify a zone here (e.g. "-a") to avoid a redundant deployment
-  location   = var.gcp_config.zone_a
-  cluster    = google_container_cluster.primary.name
-  
+  location = var.gcp_config.zone_a
+  cluster  = google_container_cluster.primary.name
+
   # use autoscaling to only create a machine when there is a deployment
   autoscaling {
     min_node_count = 0
     max_node_count = 2
   }
-  
+
   node_config {
     # use preemptible, as this saves costs
-    preemptible  = true
+    preemptible = true
     #custom machine type: 16 core, 48 GB as tsan needs more RAM
     machine_type = "n2d-custom-16-49152"
     disk_size_gb = 100
-    disk_type = "pd-ssd"
+    disk_type    = "pd-ssd"
 
     # set the premissions required for the deployment later
     oauth_scopes = [
