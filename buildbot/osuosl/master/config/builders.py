42bb0bf8f738fcc001d134ad0118a6e257bd8950
Amy Huang <a.huang.011@gmail.com>
2025-07-23 14:17:18 -0700

    Remove libc arm32 buildbot (#517)
    Remove the arm32 buildbot as it has been replaced with the qemu arm32
    buildbot.

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -2321,15 +2321,6 @@ all += [
                     depends_on_projects=['llvm', 'libc', 'clang', 'clang-tools-extra'],
                     extra_args=['--debug'])},
 
-    {'name' : 'libc-arm32-debian-dbg',
-    'tags'  : ["libc"],
-    'workernames' : ['libc-arm32-debian'],
-    'builddir': 'libc-arm32-debian-dbg',
-    'factory' : AnnotatedBuilder.getAnnotatedBuildFactory(
-                    script="libc-linux.py",
-                    depends_on_projects=['llvm', 'libc', 'clang', 'clang-tools-extra'],
-                    extra_args=['--debug'])},
-
     {'name' : 'libc-arm32-qemu-debian-dbg',
     'tags'  : ["libc"],
     'workernames' : ['libc-arm32-qemu-debian'],
