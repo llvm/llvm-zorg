3dd019ab622ffb4debb98c6fa08b8a83d2bbb503
Aiden Grossman <aidengrossman@google.com>
2025-07-31 08:56:06 -0700

    [CI] Add container image for postsubmit testing of premerge config
    This patch adds a container image and some necessary setup code so we can run
    this through buildbot.
    
    Reviewers: Keenuts, cmtice, dschuff, gburgessiv, lnihlen
    
    Reviewed By: cmtice, Keenuts
    
    Pull Request: https://github.com/llvm/llvm-zorg/pull/527

---
+++ premerge/buildbot/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script performs all the necessary setup and then starts the buildbot
+# worker.
+
+mkdir /worker
+buildbot-worker create-worker /worker \
+  lab.llvm.org:9994 \
+  $BUILDBOT_USERNAME \
+  $BUILDBOT_PASSWORD
+
+echo "Google LLVM Premerge Infra Rotation <llvm-presubmit-infra@google.com>" \
+  > /worker/info/admin
+
+{
+  echo "Premerge container (https://github.com/llvm/llvm-project/pkgs/container/ci-ubuntu-24.04)"
+  echo "GCP n2/n2d standard instances."
+} > /worker/info/host
+
+buildbot-worker start /worker
