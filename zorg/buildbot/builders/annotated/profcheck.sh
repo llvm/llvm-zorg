18eee1c5ca9c37e9b3f7d22fa1701cf4628310f2
Aiden Grossman <aidengrossman@google.com>
2025-08-21 17:31:57 +0000

    [ProfCheck] Fix paths for profcheck annotated script
    Buildbot does the checkout in BUILDBOT_ROOT/llvm-project and then does the
    build in BUILDBOT_ROOT/build. Make the script reflect this.

--- zorg/buildbot/builders/annotated/profcheck.sh
+++ zorg/buildbot/builders/annotated/profcheck.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-rm -rf build
-mkdir build
-cd build
-
 echo @@@CMake@@@
 
 cmake -GNinja \
@@ -15,9 +11,10 @@ cmake -GNinja \
   -DCMAKE_CXX_COMPILER_LAUNCHER=sccache \
   -DLLVM_LIT_ARGS='--exclude-xfail' \
   -DLLVM_ENABLE_PROFCHECK=ON \
-  ../llvm
+  ../llvm-project/llvm
 
 echo @@@Ninja@@@
 
 export LIT_XFAIL="$(cat ../llvm/utils/profcheck-xfail.txt | tr '\n' ';')"
 ninja check-llvm
+
