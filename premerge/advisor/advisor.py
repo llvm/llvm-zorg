5905ee2fc2767da445e7e5f7f483c6b348c474c1
Aiden Grossman <aidengrossman@google.com>
2025-10-13 19:17:09 +0000

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.6

---
+++ premerge/advisor/advisor.py
@@ -0,0 +1,22 @@
+import flask
+from flask import Flask
+import advisor_lib
+
+advisor_blueprint = flask.Blueprint("advisor", __name__)
+
+
+@advisor_blueprint.route("/upload", methods=["POST"])
+def upload():
+    advisor_lib.upload_failures(flask.request.json)
+    return flask.Response(status=204)
+
+
+@advisor_blueprint.route("/explain")
+def explain():
+    return advisor_lib.explain_failures(flask.request.json)
+
+
+def create_app(db_path: str):
+    app = Flask(__name__)
+    app.register_blueprint(advisor_blueprint)
+    return app
