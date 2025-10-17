7b12b9aa6a3d77f7a8d0c437f061923c75aed05c
Aiden Grossman <aidengrossman@google.com>
2025-10-16 17:22:30 -0700

    [CI] Make Advisor Autoupdate Schema
    This patch makes the premerge advisor server autoupdate the DB schema if we
    ever end up changing it. We handle this by just dropping the existing table
    as preserving all data for all time is completely unnecessary in our case.
    Unittests have also been added to ensure that the setup_db function works as
    it is supposed to.
    
    Reviewers: cmtice
    
    Reviewed By: cmtice
    
    Pull Request: https://github.com/llvm/llvm-zorg/pull/616

--- premerge/advisor/advisor_lib.py
+++ premerge/advisor/advisor_lib.py
@@ -1,4 +1,5 @@
 from typing import TypedDict
+import time
 import sqlite3
 import logging
 
@@ -21,15 +22,36 @@ class FailureUpload(TypedDict):
     failures: list[TestFailure]
 
 
+_CREATE_TABLE_CMD = "CREATE TABLE failures(source_type, base_commit_sha, source_id, test_file, failure_message)"
+
+
+def _create_failures_table(connection: sqlite3.Connection):
+    logging.info("Did not find failures table, creating.")
+    connection.execute(_CREATE_TABLE_CMD)
+    connection.commit()
+
+
 def setup_db(db_path: str) -> sqlite3.Connection:
     connection = sqlite3.connect(db_path)
     tables = connection.execute("SELECT name from sqlite_master").fetchall()
-    if "failures" not in tables:
-        logging.info("Did not find failures table, creating.")
-        connection.execute(
-            "CREATE TABLE failures(source_type, base_commit_sha, source_id, test_file, failure_message)"
-        )
-        connection.commit()
+    if ("failures",) not in tables:
+        _create_failures_table(connection)
+        return connection
+
+    table_schema = connection.execute(
+        "SELECT sql FROM sqlite_master WHERE name=?", ("failures",)
+    ).fetchone()
+    if table_schema == (_CREATE_TABLE_CMD,):
+        return connection
+
+    # The schema of the table does not match what we were expecting. Keep the
+    # current table around just in case by renaming it and recreate the
+    # failures table using the expected schema.
+    new_table_name = f"failures_old_{int(time.time())}"
+    connection.execute(f"ALTER TABLE failures RENAME TO {new_table_name}")
+    connection.commit()
+
+    _create_failures_table(connection)
     return connection
 
 
