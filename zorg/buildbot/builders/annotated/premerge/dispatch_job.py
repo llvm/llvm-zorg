fb31028277f6fa617f5cb9d1be5fda892d19aba8
Aiden Grossman <aidengrossman@google.com>
2025-10-31 21:27:45 +0000

    Revert "[CI] Disable check-lldb on premerge runners"
    This reverts commit f092dfbf5b272201e2118b122dfd8c3da27291cf.
    
    The offending commit was reverted for now, so reenabling.
    https://github.com/llvm/llvm-project/issues/165887 will have updates.

--- zorg/buildbot/builders/annotated/premerge/dispatch_job.py
+++ zorg/buildbot/builders/annotated/premerge/dispatch_job.py
@@ -97,8 +97,6 @@ def start_build(
 def start_build_linux(commit_sha: str, bucket_name: str, k8s_client) -> str:
     """Starts a pod to build/test on Linux at the specified SHA."""
     pod_name = f"build-{commit_sha}"
-    # TODO(boomanaiden154): Reenable Windows when
-    # https://github.com/llvm/llvm-project/issues/165887 is fixed.
     commands = [
         "set -ex",
         "git clone --depth 100 https://github.com/llvm/llvm-project",
@@ -112,7 +110,7 @@ def start_build_linux(commit_sha: str, bucket_name: str, k8s_client) -> str:
         "export SCCACHE_GCS_RW_MODE=READ_WRITE",
         "export SCCACHE_IDLE_TIMEOUT=0",
         "sccache --start-server",
-        './.ci/monolithic-linux.sh "bolt;clang;clang-tools-extra;flang;libclc;lld;lldb;llvm;mlir;polly" "check-bolt check-clang check-clang-tools check-flang check-lld check-llvm check-mlir check-polly check-lit" "compiler-rt;flang-rt;libc;libcxx;libcxxabi;libunwind" "check-compiler-rt check-flang-rt check-libc" "check-cxx check-cxxabi check-unwind" "OFF"',
+        './.ci/monolithic-linux.sh "bolt;clang;clang-tools-extra;flang;libclc;lld;lldb;llvm;mlir;polly" "check-bolt check-clang check-clang-tools check-flang check-lld check-lldb check-llvm check-mlir check-polly check-lit" "compiler-rt;flang-rt;libc;libcxx;libcxxabi;libunwind" "check-compiler-rt check-flang-rt check-libc" "check-cxx check-cxxabi check-unwind" "OFF"',
         "python .ci/cache_lit_timing_files.py upload",
         "echo BUILD FINISHED",
     ]
