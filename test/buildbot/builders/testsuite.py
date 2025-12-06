2c8553b41eeab8004731e311e696cfed40ebf979
Vladimir Vereschaka <vvereschaka@accesssoftek.com>
2025-12-06 14:04:50 -0800

    Update TestSuiteBuilder.getLlvmTestSuiteSteps factory. (#656)
    Added new optional arguments:
    * compiler_flags - common flags for C and C++ compilers.
    * linker_flags - common linker flags for all exe/module/shared
    configurations.
    * src_dir - source dir for the test suite source code (within
    prop:buildir).
    * obj_dir - build dir for the test suite (within prop:buildir).
    
    Added new output properties:
    * ts_srcdir
    * ts_objdir
    
    The 'compiler_flags' argument extends or adds the following CMake
    configuration parameters to the appropriate step: CMAKE_C_FLAGS and
    CMAKE_CXX_FLAGS. The 'linker_flags' argument extends or adds the
    following CMake configuration parameters accordingly:
    CMAKE_EXE_LINKER_FLAGS, CMAKE_MODULE_LINKER_FLAGS and
    CMAKE_SHARED_LINKER_FLAGS.

--- test/buildbot/builders/testsuite.py
+++ test/buildbot/builders/testsuite.py
@@ -24,27 +24,43 @@ f = TestSuiteBuilder.getLlvmTestSuiteSteps(
 
 print(f"default factory: {f}\n")
 
-assert factory_has_num_steps(f, 5)
+assert factory_has_num_steps(f, 6)
 assert factory_has_step(f, "clean-src-dir")
+assert factory_has_step(f, "set-props")
 assert factory_has_step(f, "cmake-configure")
 assert factory_has_step(f, "build-default")
 assert not factory_has_step(f, "rsync-default")
 assert factory_has_step(f, "test-check")
 
+compiler_flags_ = "-march=armv8l+pauth -mbranch-protection=pac-ret -O2"
+linker_flags_ = "-O2 -Wl,--emit-relocs"
+
 f = TestSuiteBuilder.getLlvmTestSuiteSteps(
         cmake_definitions = {
             "TEST_SUITE_REMOTE_HOST"    : "buildbot@arm64-linux-02",
             "TEST_SUITE_LIT_FLAGS"      : "-v --threads=32 --time-tests",
+            "CMAKE_CXX_FLAGS"           : "-O0",
+            "CMAKE_EXE_LINKER_FLAGS"    : "-O0",
         },
         compiler_dir = util.Interpolate("%(prop:builddir)s/build"),
+        compiler_flags = compiler_flags_,
+        linker_flags = linker_flags_,
         hint = None,
     )
     
 print(f"default factory (compiler_dir): {f}\n")
 
-assert factory_has_num_steps(f, 6)
+assert factory_has_num_steps(f, 7)
 assert factory_has_step(f, "clean-src-dir")
+assert factory_has_step(f, "set-props")
 assert factory_has_step(f, "cmake-configure")
+assert factory_has_step(f, "cmake-configure", hasarg = "definitions", contains = {
+                                                                        "CMAKE_C_FLAGS"             : compiler_flags_,
+                                                                        "CMAKE_CXX_FLAGS"           : f"-O0 {compiler_flags_}",
+                                                                        "CMAKE_EXE_LINKER_FLAGS"    : f"-O0 {linker_flags_}",
+                                                                        "CMAKE_MODULE_LINKER_FLAGS" : linker_flags_,
+                                                                        "CMAKE_SHARED_LINKER_FLAGS" : linker_flags_,
+                                                                    })
 assert factory_has_step(f, "build-default")
 assert factory_has_step(f, "rsync-default")
 assert factory_has_step(f, "test-check")
