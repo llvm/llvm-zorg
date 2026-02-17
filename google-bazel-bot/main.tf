356517ccc465f089d6532557b61e626a07cbde55
Aiden Grossman <aidengrossman@google.com>
2026-02-17 22:21:57 +0000

    [𝘀𝗽𝗿] changes to main this commit is based on
    Created using spr 1.3.6
    
    [skip ci]

--- google-bazel-bot/main.tf
+++ google-bazel-bot/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "6.43.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.35.1"
+    }
   }
 }
 
@@ -40,4 +44,66 @@ resource "local_file" "terraform_state" {
     }
   }
   EOT
+}
+
+resource "google_container_cluster" "llvm_bazel_cluster" {
+  name     = "llvm-bazel-cluster"
+  location = "us-central1-c"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "bazel_ci" {
+  name               = "bazel-ci"
+  location           = "us-central1-c"
+  cluster            = google_container_cluster.llvm_bazel_cluster.name
+  initial_node_count = 2
+
+  node_config {
+    machine_type = "n2-standard-64"
+  }
+}
+
+data "google_client_config" "current" {}
+
+provider "kubernetes" {
+  host  = "https://${llvm_bazel_cluster.endpoint}"
+  token = data.google_client_config.current.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.llvm_bazel_cluster.cluster_ca_certificate
+  )
+  alias = "llvm-bazel-cluster"
+}
+
+data "google_secret_manager_secret_version" "buildkite_agent_token" {
+  secret = "buildkite-agent-token"
+}
+
+resource "kubernetes_namespace" "bazel_ci" {
+  metadata {
+    name = "bazel-ci"
+  }
+}
+
+resource "kubernetes_secret" "buildkite_agent_token" {
+  metadata {
+    name      = "buildkite-agent-token"
+    namespace = kubernetes_namespace.bazel_ci.metadata[0].name
+  }
+
+  data = {
+    "token" = data.google_secret_manager_secret_version.buildkite_agent_token.secret_data
+  }
+
+  type       = "Opaque"
+  depends_on = [kubernetes_namespace.bazel_ci]
+}
+
+resource "kubernetes_manifest" "bazel_buildkite" {
+  manifest = yamldecode(file("bazel-buildkite.yaml"))
+  depends_on = [
+    kubernetes_namespace.bazel_ci,
+    kubernetes_secret.buildkite_agent_token
+  ]
 }
