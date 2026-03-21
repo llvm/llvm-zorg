19321d24ffcf9bae0b93fa3eae83487a843bc2dc
Vitaly Buka <vitalybuka@google.com>
2026-03-21 13:27:39 -0700

    Update pip install command to include Pygments and dev

--- zorg/buildbot/builders/sanitizers/buildbot_functions.sh
+++ zorg/buildbot/builders/sanitizers/buildbot_functions.sh
@@ -83,7 +83,7 @@ export LIT_OPTS="--time-tests"
 [[ -f llvm-venv/bin/activate ]] || python3 -m venv llvm-venv
 . llvm-venv/bin/activate
 pip install --upgrade pip
-pip install psutil pyyaml
+pip install psutil pyyaml Pygments dev
 
 CMAKE_COMMON_OPTIONS+=" -DPython3_EXECUTABLE=$ROOT/llvm-venv/bin/python"
 
