c3489465f395bcc52838d2d86f22747367430c9a
Aiden Grossman <aidengrossman@google.com>
2026-05-25 19:30:26 -0700

    [google-integrate] Move workers to production buildmaster

--- buildbot/google/scripts/google-integrate.sh
+++ buildbot/google/scripts/google-integrate.sh
@@ -25,7 +25,7 @@ ln -sf /usr/bin/ld.lld-${LLVM_MAJOR_VERSION} /usr/bin/ld
 
 rm -rf /b
 BOT_DIR=/b
-SERVER_PORT=9994
+SERVER_PORT=9990
 WORKER_NAME="$(hostname -s)"
 WORKER_PASSWORD="$(gsutil cat gs://sanitizer-buildbot/$(hostname -s)-password)"
 
