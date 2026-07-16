llvm/llvm-zorg

commit 82a0d6fcde70fe795eccfc2267364c8f1f13743a

[BazelBot] Enable local testing of bot (#885)
This stopped working when support for GitHub app was added

google-bazel-bot/bazel-bot/utils.py
@@ -67,14 +67,25 @@ class BazelBuildResult:
 
 
 class CredentialManager:
-    def __init__(self):
+    def __init__(self, use_github_app: bool):
         self.gh_fork_user = os.getenv("GITHUB_FORK_USER")
         self.gh_pr_user = os.getenv("GITHUB_PR_USER")
         self.bk_token = os.getenv("BUILDKITE_API_TOKEN")
+        self.use_github_app = use_github_app
+
+        # GitHub app details
         self.gh_app_id = os.getenv("GITHUB_APP_ID")
         self.gh_app_private_key = os.getenv("GITHUB_APP_PRIVATE_KEY")
         self.gh_pr_app_id = os.getenv("GITHUB_PR_APP_ID")
         self.gh_pr_app_private_key = os.getenv("GITHUB_PR_APP_PRIVATE_KEY")
+
+        # Used when not using GitHub app
+        self.gh_fork_user_token = os.getenv("GITHUB_FORK_USER_TOKEN")
+        self.gh_pr_user_token = os.getenv("GITHUB_PR_USER_TOKEN")
+        if not self.use_github_app and not self.gh_fork_user_token:
+            raise EnvironmentError(
+                "GITHUB_FORK_USER_TOKEN must be set when not using GitHub app"
+            )
 
     @property
     def gh_fork_repo_name(self):
@@ -226,28 +237,35 @@ class LocalGitRepo:
                 self.repo_path,
             )
         self.repo = git.Repo(repo_path)
-        self.fork_github_integration = github.GithubIntegration(
-            auth=github.Auth.AppAuth(creds.gh_app_id, creds.gh_app_private_key)
-        )
-        self.gh_fork_installation = self.fork_github_integration.get_repo_installation(
-            self.creds.gh_fork_user, "llvm-project"
-        )
-        self.gh_fork_repo = (
-            self.gh_fork_installation.get_github_for_installation().get_repo(
-                self.creds.gh_fork_repo_name
+
+        if self.creds.use_github_app:
+            self.fork_github_integration = github.GithubIntegration(
+                auth=github.Auth.AppAuth(creds.gh_app_id, creds.gh_app_private_key)
             )
-        )
-        self.pr_github_integration = github.GithubIntegration(
-            auth=github.Auth.AppAuth(creds.gh_pr_app_id, creds.gh_pr_app_private_key)
-        )
-        self.gh_pr_installation = self.pr_github_integration.get_repo_installation(
-            self.creds.gh_pr_user, "llvm-project"
-        )
-        self.gh_pr_repo = (
-            self.gh_pr_installation.get_github_for_installation().get_repo(
-                self.creds.gh_pr_repo_name
+            self.gh_fork_installation = self.fork_github_integration.get_repo_installation(
+                self.creds.gh_fork_user, "llvm-project"
             )
-        )
+            self.gh_fork_repo = (
+                self.gh_fork_installation.get_github_for_installation().get_repo(
+                    self.creds.gh_fork_repo_name
+                )
+            )
+
+            self.pr_github_integration = github.GithubIntegration(
+                auth=github.Auth.AppAuth(creds.gh_pr_app_id, creds.gh_pr_app_private_key)
+            )
+            self.gh_pr_installation = self.pr_github_integration.get_repo_installation(
+                self.creds.gh_pr_user, "llvm-project"
+            )
+            self.gh_pr_repo = (
+                self.gh_pr_installation.get_github_for_installation().get_repo(
+                    self.creds.gh_pr_repo_name
+                )
+            )
+        else:
+            self.gh_fork_repo = github.Github(self.creds.gh_fork_user_token).get_repo(self.creds.gh_fork_repo_name)
+            self.gh_pr_repo = github.Github(self.creds.gh_pr_user_token).get_repo(self.creds.gh_pr_repo_name) if self.can_create_pr else None
+
         self.bazel_utils_path = os.path.join(self.repo_path, "utils", "bazel")
         self.main_branch = "main"
         self.remote_name = "origin"
@@ -316,11 +334,15 @@ class LocalGitRepo:
 
         # 1. Get access token
         try:
-            access_token = call_with_retry(
-                (github.GithubException,),
-                self.fork_github_integration.get_access_token,
-                self.gh_fork_installation.id,
-            ).token
+            access_token = (
+                call_with_retry(
+                    (github.GithubException,),
+                    self.fork_github_integration.get_access_token,
+                    self.gh_fork_installation.id,
+                ).token
+                if self.creds.use_github_app
+                else self.creds.gh_fork_user_token
+            )
         except github.GithubException as e:
             logger.error(f"Failed to get access token: {e}")
             return False
@@ -377,6 +399,11 @@ class LocalGitRepo:
         )
 
         try:
+            if create_pr and not self.creds.gh_pr_user_token:
+                raise EnvironmentError(
+                    "GITHUB_PR_USER_TOKEN must be set to create PR when not using GitHub App."
+                )
+
             pr = call_with_retry(
                 (github.GithubException,),
                 self.gh_pr_repo.create_pull,

google-bazel-bot/bazel-bot/bazelbot_server.py
@@ -151,15 +151,23 @@ class BazelRepairBot:
                 break
 
             self.git_repo.create_branch_for_fix(build_data.commit)
+
+            # For local testing, users must use their personal access tokens
+            access_token = (
+                self.git_repo.fork_github_integration.get_access_token(
+                    self.git_repo.gh_fork_installation.id
+                )
+                if self.creds.use_github_app
+                else self.creds.gh_fork_user_token
+            )
+
             # We pass the local path so the agent can read files, and the commit to analyze
             agent_result = asyncio.run(
                 bazel_agent.query_agent(
                     commit_sha=build_data.commit,
                     cmd_processor=self.command_processor,
                     past_fixes=past_fixes[:],
-                    github_token=self.git_repo.fork_github_integration.get_access_token(
-                        self.git_repo.gh_fork_installation.id
-                    ),
+                    github_token=access_token,
                 )
             )
 

google-bazel-bot/bazel-bot/bazelbot_server_main.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
         format="%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s",
         handlers=handlers,
     )
-    creds_manager = utils.CredentialManager()
+    creds_manager = utils.CredentialManager(use_github_app=not args.test_commits)
     cmd_processor = utils.CommandProcessor(args.llvm_git_repo)
     git_repo = utils.LocalGitRepo(args.llvm_git_repo, creds_manager, args.create_prs)
     build_processor = utils.LocalBuildProcessor(cmd_processor, git_repo)

google-bazel-bot/bazel-bot/bazelbot_server_test.py
@@ -32,7 +32,7 @@ class TestBazelBotServer(unittest.TestCase):
         },
     )
     def test_credential_manager(self):
-        creds = utils.CredentialManager()
+        creds = utils.CredentialManager(use_github_app=True)
         self.assertEqual(creds.gh_fork_user, "fork_user")
         self.assertEqual(creds.gh_pr_user, "pr_user")
         self.assertEqual(creds.bk_token, "bk_token")

commit 0c714ace71d7be433dd234e06570755b4151a2f2

[BazelBot] Github App setup doesn't need GITHUB_PR_USER_TOKEN set
Bug introduced in #885

google-bazel-bot/bazel-bot/utils.py
@@ -399,10 +399,11 @@ class LocalGitRepo:
         )
 
         try:
-            if create_pr and not self.creds.gh_pr_user_token:
-                raise EnvironmentError(
-                    "GITHUB_PR_USER_TOKEN must be set to create PR when not using GitHub App."
-                )
+            if not self.creds.use_github_app:
+                if create_pr and not self.creds.gh_pr_user_token:
+                    raise EnvironmentError(
+                        "GITHUB_PR_USER_TOKEN must be set to create PR when not using GitHub App."
+                    )                
 
             pr = call_with_retry(
                 (github.GithubException,),