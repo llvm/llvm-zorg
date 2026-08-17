3a40221ae04435a668e52da1e4ef8841b12ae9a6
Bhavesh Mandalapu <bmandalapu@google.com>
2026-08-17 16:59:47 -0700

    Bring back building mlir opt and translate being built as deps before llvm

--- zorg/buildbot/builders/annotated/mlgo-emitc.sh
+++ zorg/buildbot/builders/annotated/mlgo-emitc.sh
@@ -17,7 +17,7 @@ set -o pipefail
 
 halt_on_failure
 
-build_step "Build LLVM"
+build_step "Build MLIR Dependencies (mlir-opt and mlir-translate)"
 
 mkdir -p deps
 cd deps
@@ -27,11 +27,27 @@ cmake -GNinja \
   -B . \
   -DCMAKE_BUILD_TYPE="Release" \
   -DLLVM_CCACHE_BUILD=ON \
+  -DLLVM_ENABLE_PROJECTS="mlir"
+
+ninja mlir-opt mlir-translate
+
+MLIR_OPT="$(pwd)/bin/mlir-opt"
+MLIR_TRANSLATE="$(pwd)/bin/mlir-translate"
+
+cd ..
+
+build_step "Build LLVM"
+
+cmake -GNinja \
+  -S ../llvm-project/llvm \
+  -B . \
+  -DCMAKE_BUILD_TYPE="Release" \
+  -DLLVM_CCACHE_BUILD=ON \
   -DLLVM_ENABLE_ASSERTIONS=ON \
   -DLLVM_IR2VEC_ENABLE_PYTHON_BINDINGS=ON \
-  "-DLLVM_MLGO_MODELS=inliner,../../llvm-project/mlir/test/Integration/Dialect/EmitC/inline-oz-test-model-tosa.mlir,inliner;regalloc,../../llvm-project/mlir/test/Integration/Dialect/EmitC/regalloc-eviction-test-model-tosa.mlir,regalloc" \
-  -DLLVM_MLGO_MLIR_OPT="bin/mlir-opt" \
-  -DLLVM_MLGO_MLIR_TRANSLATE="bin/mlir-translate" \
+  "-DLLVM_MLGO_MODELS=inliner,../llvm-project/mlir/test/Integration/Dialect/EmitC/inline-oz-test-model-tosa.mlir,inliner;regalloc,../llvm-project/mlir/test/Integration/Dialect/EmitC/regalloc-eviction-test-model-tosa.mlir,regalloc" \
+  -DLLVM_MLGO_MLIR_OPT="${MLIR_OPT}" \
+  -DLLVM_MLGO_MLIR_TRANSLATE="${MLIR_TRANSLATE}" \
   '-DLLVM_LIT_ARGS=-v -vv'
 
 build_step "Run tests"
